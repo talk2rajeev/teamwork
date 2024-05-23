a99fad1ccd3b9c920e955f64ef68297b3bdef580
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-05-23 16:14:43 +0530

    formatter func moved here

--- server/utils/formatter.js
+++ server/utils/formatter.js
@@ -9,3 +9,34 @@ export function formatRolesList(roles) {
         }
     }))
 }
+
+export function getFormattedproduct(products) {
+    return products.map(p => {
+        const {productId, productName, profileId, fname, lname, teamId, teamName} = p;
+        return {
+            productId,
+            productName,
+            createdBy: {
+                profileId,
+                fname,
+                lname
+            },
+            team: {
+                teamId,
+                teamName
+            }
+        }
+    });
+}
+
+export function getFormattedproductWithTeamUsers(products, teamsWithUsers) {
+    return products.map(p => {
+        return {
+            ...p,
+            team: {
+                ...p.team,
+                teamsWithUsers
+            }
+        }
+    });
+}
