4785d4d913e955d60e8b194d91925db8767c34a0
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-06-06 10:29:37 +0530

    teamservice test cases query as variable

--- server/services/teamService/__test__/team.service.test.js
+++ server/services/teamService/__test__/team.service.test.js
@@ -1,5 +1,7 @@
 import {pool} from '../../../config/dbConfig/database.js';
 import * as teamService from '../team.service.js';
+import {SQL_QUERIES} from '../../../utils/queries.js';
+
 
 // Mock environment values
 jest.mock('../../../config/dbConfig/database.js', () => ({
@@ -18,7 +20,7 @@ jest.mock('../../../config/dbConfig/database.js', () => ({
         const teamId = 1;
         const teamData = await teamService.getTeamById(teamId);
   
-        expect(pool.query).toHaveBeenCalledWith(`select * from team where teamId = ?`, [userId]);
+        expect(pool.query).toHaveBeenCalledWith(SQL_QUERIES.team.getTeamByIdQuery, [teamId]);
         expect(teamData).toEqual(mockUserData);
       });
 
@@ -29,20 +31,20 @@ jest.mock('../../../config/dbConfig/database.js', () => ({
         pool.query.mockRejectedValueOnce(new Error(errorMessage));
   
         const teamId = 1;
-        await expect(teamService.getTeamById(teamId)).rejects.toThrow(`Failed to get Team by id: ${errorMessage}`);
+        await expect(teamService.getTeams(teamId)).rejects.toThrow(`Failed to get teams: ${errorMessage}`);
       });
     });
 
-    describe('getAllTeams', () => {
+    describe('getTeams', () => {
         it('should return team list', async () => {
           // Mock query result
-          const mockUserData = [{"teamId": 1, "teamName": "NEXT GEN TEAM", "createdAt": "2024-02-10T13:21:45.184Z"}, {"teamId": 2, "teamName": "OPDS", "createdAt": "2024-02-04T13:20:14.184Z"}];
-          pool.query.mockResolvedValueOnce([mockUserData]);
+          const mockTeamData = [{"teamId": 1, "teamName": "NEXT GEN TEAM", "createdAt": "2024-02-10T13:21:45.184Z"}, {"teamId": 2, "teamName": "OPDS", "createdAt": "2024-02-04T13:20:14.184Z"}];
+          pool.query.mockResolvedValueOnce([mockTeamData]);
     
-          const teamData = await userService.getUsers();
+          const teamData = await teamService.getTeams();
     
-          expect(pool.query).toHaveBeenCalledWith('SELECT * from team');
-          expect(teamData).toEqual(mockUserData);
+          expect(pool.query).toHaveBeenCalledWith(SQL_QUERIES.team.getTeamsQuery);
+          expect(teamData).toEqual(mockTeamData);
         });
   
     
@@ -51,7 +53,7 @@ jest.mock('../../../config/dbConfig/database.js', () => ({
           const errorMessage = 'Database error';
           pool.query.mockRejectedValueOnce(new Error(errorMessage));
     
-          expect(pool.query).toHaveBeenCalledWith('SELECT * from team');
+          expect(pool.query).toHaveBeenCalledWith(SQL_QUERIES.team.getTeamsQuery);
           await expect(teamService.getTeams()).rejects.toThrow(`Failed to get teams: ${errorMessage}`);
         });
       });
