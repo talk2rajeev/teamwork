6d8434c3f1240913aecef909c82365ef61d8a0ae
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-06-26 10:18:21 +0530

    dateInput component

--- client-react-redux/src/components/core-components/index.tsx
+++ client-react-redux/src/components/core-components/index.tsx
@@ -1,9 +1,11 @@
 import Button from "./button/Button";
 import Input from "./input/Input";
 import TextArea from "./textArea/TextArea";
+import DateInput from "./dateInput/dateInput";
 
 export {
     Button,
     Input,
-    TextArea
+    TextArea,
+    DateInput
 };
