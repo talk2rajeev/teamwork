1917117cdea8b962277218b686d2508ef7824caa
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-05 14:06:02 +0530

    product module

---
+++ client-react-redux/src/components/appComponents/createProduct/createProduct.tsx
@@ -0,0 +1,72 @@
+import react, {useState} from 'react';
+import axios from 'axios';
+import * as coreComponents from '../../core-components';
+
+type CreateProductProps = {}
+
+
+const CreateProduct: React.FC<CreateProductProps> = () => {
+
+    // TODO: create product
+    // assign product to team
+    
+    const [reqPayload, setReqPayload] = useState<string>();
+
+
+    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>, data?: any) => {
+        console.log(event.target.name, event.target.value);
+    }
+
+    const onDescriptionChange = (event: React.ChangeEvent<HTMLTextAreaElement>, data?: any) => {
+
+    }
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        try {
+            const requestPaylod = { data: 'null' };
+            await axios.post('http://localhost:3000/save-post', requestPaylod);
+            console.log(requestPaylod, 'Post saved successfully');
+        } catch (error) {
+            console.error('Error saving post:', error);
+            alert('Error saving post');
+        }
+    };
+
+    
+    return <div className='user-story-container bg-white p-2'>
+        <div className='p-2 mb-4'>
+            <coreComponents.Input 
+                label='Product Name'
+                type='text'
+                name='title'
+                placeholder='Product name'
+                onchange={onInputChange}
+                classes='text-base bg-white border-1 border-slate-300 outline-slate-400'
+            />
+        </div>
+
+        <div className='p-2 mb-4'>
+            <coreComponents.TextArea 
+                label='Product Name'
+                name='title'
+                placeholder='Product name'
+                onchange={onDescriptionChange}
+                classes='text-base bg-white border-1 border-slate-300 outline-slate-400'
+            />
+        </div>
+
+        <div className='mb-2'>
+            <div>
+                <coreComponents.Button 
+                    label="Submit"
+                    type="primary"
+                    clickHandler={handleSubmit}
+                    disabled={true}
+                />
+            </div>
+        </div>
+    </div>
+}
+
+export default CreateProduct;
