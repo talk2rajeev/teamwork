d170fd7b50b6f95d0b1337d753788eba047d63c7
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-05 14:06:25 +0530

    protexted route update

---
+++ client-react-redux/src/components/appComponents/protectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+interface ProtectedRouteProps {
+  isAuthenticated: boolean;
+  redirectPath?: string;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ isAuthenticated, redirectPath = '/' }) => {
+  return isAuthenticated ? <Outlet /> : <Navigate to={redirectPath} />;
+};
+
+export default ProtectedRoute;
