61c318ceccac09b99024f5672951df7bb59ac6f1
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-16 00:14:51 +0530

    auth controller/service refactored

--- server/controllers/authController/auth.controller.js
+++ server/controllers/authController/auth.controller.js
@@ -1,61 +1,63 @@
-import jwt from 'jsonwebtoken';
-import * as authService from '../../services/authService/auth.service.js';
-import { comparePasswrod } from '../../utils/bcrypt.js'
-import {EXPIRE_TIME} from '../../utils/constants.js';
-import {generateAccessToken, generateRefreshToken} from '../../utils/tokenGenerator.js';
-import { getEnvValue } from '../../utils/envValue.js';
-import moment from 'moment';
-
+import jwt from "jsonwebtoken";
+import * as authService from "../../services/authService/auth.service.js";
+import { comparePasswrod } from "../../utils/bcrypt.js";
+import { EXPIRE_TIME } from "../../utils/constants.js";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../../utils/tokenGenerator.js";
+import { getEnvValue } from "../../utils/envValue.js";
+import moment from "moment";
 
 async function login(req, res) {
-    const { username, password } = req.body;
-    
-    const users = await authService.findUserByUsername(username);
-    const user = users.find(u => u.username === username);
-    
-    if (!user || !comparePasswrod(password, user.password)) {
-      return res.status(401).json({ message: 'Invalid username or password' });
-    }
+  const { username, password } = req.body;
 
-    const userWithDetail = await authService.getUserDetail(user.loginId);
+  const users = await authService.findUserByUsername(username);
+  const user = users.find((u) => u.username === username);
 
-    const userTeams = userWithDetail.map(u => ({teamId: u.teamId, teamName: u.teamName, roleId: u.roleId, roleName: u.roleName}));
-    const fname = userWithDetail[0]?.fname;
-    const lname = userWithDetail[0]?.lname;
-   
-    
-    delete user.password; 
-    const expireTime = moment().add(EXPIRE_TIME, 'seconds').valueOf();
-    const expiresIn = `${EXPIRE_TIME} seconds`;
-    const userData = {...user, fname, lname, userTeams, expireTime, expiresIn};
+  if (!user || !comparePasswrod(password, user.password)) {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
 
-    const accessToken = generateAccessToken({ ...userData });
-    const refreshToken = generateRefreshToken({ ...userData });
-    
+  const userWithDetail = await authService.getUserDetail(user.loginId);
 
-    res
+  const expireTime = moment().add(EXPIRE_TIME, "seconds").valueOf();
+  const expiresIn = `${EXPIRE_TIME} seconds`;
+  const userData = { ...userWithDetail[0], expireTime, expiresIn };
+
+  const accessToken = generateAccessToken({ ...userData });
+  const refreshToken = generateRefreshToken({ ...userData });
+
+  res
     // .cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'strict' })
     // .header('Authorization', accessToken)
-    .json({ accessToken, refreshToken, expireTime, expiresIn: `${EXPIRE_TIME} seconds` });
-};
+    .json({
+      accessToken,
+      refreshToken,
+      expireTime,
+      expiresIn: `${EXPIRE_TIME} seconds`,
+    });
+}
 
 function token(req, res) {
-    // const refreshToken = req.body.token;
-    const refreshToken = req.cookies['refreshToken'];
-    if (!refreshToken) {
-      return res.status(401).send('Access Denied. No refresh token provided.');
-    }
+  // const refreshToken = req.body.token;
+  const refreshToken = req.cookies["refreshToken"];
+  if (!refreshToken) {
+    return res.status(401).send("Access Denied. No refresh token provided.");
+  }
 
-    try {
-      const decoded = jwt.verify(refreshToken, 'REFRESH_TOKEN_SECRET');
-      const accessToken = jwt.sign({ user: decoded.user }, 'ACCESS_TOKEN_SECRET', { expiresIn: '1h' });
-  
-      res
-        .header('Authorization', accessToken)
-        .send(decoded.user);
-    } catch (error) {
-      return res.status(400).send('Invalid refresh token.');
-    }
-};
+  try {
+    const decoded = jwt.verify(refreshToken, "REFRESH_TOKEN_SECRET");
+    const accessToken = jwt.sign(
+      { user: decoded.user },
+      "ACCESS_TOKEN_SECRET",
+      { expiresIn: "1h" }
+    );
+
+    res.header("Authorization", accessToken).send(decoded.user);
+  } catch (error) {
+    return res.status(400).send("Invalid refresh token.");
+  }
+}
 
 export { login, token };
--- server/services/authService/auth.service.js
+++ server/services/authService/auth.service.js
@@ -1,20 +1,36 @@
-import {pool} from '../../config/dbConfig/database.js';
+import { pool } from "../../config/dbConfig/database.js";
 
 async function findUserByUsername(username) {
-    const [users, ...meta] = await pool.query(`select * from user_login where username = ?`, [username]).catch(err => console.log(err));
-    return users;
+  const [users, ...meta] = await pool
+    .query(`select * from user_login where username = ?`, [username])
+    .catch((err) => console.log(err));
+  return users;
 }
 
-async function getUserDetail(loginId) {
-    const query = `SELECT up.fname, up.lname, t.teamName, t.teamId, r.roleName, r.roleId, ul.username
+async function getUserTeamDetail(loginId) {
+  const query = `SELECT up.fname, up.lname, t.teamName, t.teamId, r.roleName, r.roleId, ul.username
         FROM user_profile up
         JOIN  user_login ul ON up.loginId = ul.loginId 
         JOIN user_team_role utr ON up.profileId = utr.profileId
         JOIN team t ON utr.teamId = t.teamId
         JOIN role r ON utr.roleId = r.roleId
-        WHERE ul.loginId = ?`
-    const [users, ...meta] = await pool.query(query, [loginId]).catch(err => console.log(err));
-    return users;
+        WHERE ul.loginId = ?`;
+  const [users, ...meta] = await pool
+    .query(query, [loginId])
+    .catch((err) => console.log(err));
+  return users;
 }
 
-export { findUserByUsername, getUserDetail }
+async function getUserDetail(loginId) {
+  const query = `SELECT up.fname, up.lname, ul.username, r.roleName, r.roleId
+        FROM user_profile up
+        JOIN  user_login ul ON up.loginId = ul.loginId 
+        JOIN role r ON r.roleId = up.role_id
+        WHERE up.loginId = ?`;
+  const [users, ...meta] = await pool
+    .query(query, [loginId])
+    .catch((err) => console.log(err));
+  return users;
+}
+
+export { findUserByUsername, getUserDetail };
