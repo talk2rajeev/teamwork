7ddfff4b5a536c9aa94bea84e10f044409952a4d
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-15 19:51:32 +0530

    Query modified

--- server/utils/queries.js
+++ server/utils/queries.js
@@ -1,33 +1,50 @@
 export const SQL_QUERIES = {
-    product: {
-        insertWithTeamQuery: 'insert into product (productName, createdById, teamId) values(?,?,?)',
-        insertWithoutTeamQuery: 'insert into product (productName, createdById) values(?,?)',
-        getProductsQuery: `SELECT * from product`,
-        getProductsWithTeamQuery: `SELECT product.productId, product.productName, 
-            user_profile.profileId, user_profile.fname, user_profile.lname,
-            team.teamId, team.teamName
-            FROM  product
-            JOIN  user_profile ON product.createdById = user_profile.profileId
-            JOIN team ON product.teamId = team.teamId;`,
-        getProductByIdQuery: `SELECT product.productId, product.productName, 
-            user_profile.profileId, user_profile.fname, user_profile.lname,
-            team.teamId, team.teamName
-            FROM  product
-            JOIN  user_profile ON product.createdById = user_profile.profileId
-            JOIN team ON product.teamId = team.teamId WHERE productId = ?`,
-        updateProductName: `Update product SET productName = ? WHERE productId = ?`,
-        updateProductTeam: `Update product SET teamId = ? WHERE productId = ?`,
-        updateProductNameAndTeam: `Update product SET productName = ?, teamId = ? WHERE productId = ?`,
-    },
-    user: {
-        createUserLogin: 'insert into users (username, password, createdAt) values(?,?,?)',
-        createUserProfile: 'insert into user_profile (fname, lname, roleId, userId, createdAt) values(?,?,?,?,?)',
-    },
-    team: {
-        insertQuery: `insert into team (teamName, createdById) values(?, ?)`,
-        getTeamsQuery: `select * from team`,
-        getTeamByIdQuery: `select * from team where teamId = ?`,
-        getTeamWithUsersByIdQuery: `SELECT
+  product: {
+    insertWithTeamQuery:
+      "insert into product (productName, createdById, teamId, product_owner_id) values(?,?,?,?)",
+    insertWithoutTeamQuery:
+      "insert into product (productName, createdById, product_owner_id) values(?,?,?)",
+    getProductsQuery: `select product.productId, product.productName, product.product_owner_id,
+            user_profile.fname as product_owner_fname, user_profile.lname  as product_owner_lname
+            FROM product
+            JOIN  user_profile ON user_profile.profileId = product.product_owner_id;`,
+    getProductsWithTeamQuery: `SELECT p.productId, p.productName, p.product_owner_id,
+            up.profileId, up.fname, up.lname,
+            upr.profileId as owner_profileID, upr.fname as owner_fname, upr.lname as owner_lname,
+            t.teamId, t.teamName
+            FROM  product p
+            JOIN  user_profile up ON p.createdById = up.profileId
+            JOIN user_profile upr ON p.product_owner_id = upr.profileId
+            JOIN team t ON p.teamId = t.teamId;`,
+    getProductByIdQuery: `SELECT p.productId, p.productName, p.product_owner_id,
+            up.profileId, up.fname, up.lname,
+            upr.profileId as owner_profileID, upr.fname as owner_fname, upr.lname as owner_lname,
+            t.teamId, t.teamName
+            FROM  product p
+            JOIN  user_profile up ON p.createdById = up.profileId
+            JOIN user_profile upr ON p.product_owner_id = upr.profileId
+            JOIN team t ON p.teamId = t.teamId
+            WHERE productId = ?`,
+    updateProductName: `Update product SET productName = ? WHERE productId = ?`,
+    updateProductTeam: `Update product SET teamId = ? WHERE productId = ?`,
+    updateProductOwner: `Update product SET product_owner_id = ? WHERE productId = ?`,
+    updateProductNameAndTeam: `Update product SET productName = ?, teamId = ?, product_owner_id = ? WHERE productId = ?`,
+  },
+  user: {
+    createUserLogin:
+      "insert into users (username, password, createdAt) values(?,?,?)",
+    createUserProfile:
+      "insert into user_profile (fname, lname, roleId, userId, createdAt) values(?,?,?,?,?)",
+    getAllUsers: `SELECT user_login.loginId, user_login.username, user_profile.fname, user_profile.lname, role.roleId, role.roleName
+                FROM user_login 
+                JOIN user_profile ON user_login.loginId=user_profile.loginId
+                JOIN role ON user_profile.role_id=role.roleId;`,
+  },
+  team: {
+    insertQuery: `insert into team (teamName, createdById) values(?, ?)`,
+    getTeamsQuery: `select * from team`,
+    getTeamByIdQuery: `select * from team where teamId = ?`,
+    getTeamWithUsersByIdQuery: `SELECT
                 up.profileId,
                 up.fname,
                 up.lname,
@@ -41,41 +58,45 @@ export const SQL_QUERIES = {
                 team t ON utr.teamId = t.teamId
             WHERE
                 t.teamId = ?`,
-        updateTeamQuery: `Update team SET teamName = ? WHERE teamId = ?`,
-        assignRoleToUserInTeamQuery: `insert into user_team_role (teamId, profileId, roleId) values(?, ?, ?)`,
-        updateUserRoleInTeamQuery: `update user_team_role SET roleId = ? WHERE userTeamRoleId = ?`,
-        getAllTeamsWithUsersQuery: `SELECT up.fname AS first_name, up.lname AS last_name, t.teamName AS team_name, r.roleName AS role_name
+    updateTeamQuery: `Update team SET teamName = ? WHERE teamId = ?`,
+    assignRoleToUserInTeamQuery: `insert into user_team_role (teamId, profileId, roleId) values(?, ?, ?)`,
+    updateUserRoleInTeamQuery: `update user_team_role SET roleId = ? WHERE userTeamRoleId = ?`,
+    getAllTeamsWithUsersQuery: `SELECT up.fname AS first_name, up.lname AS last_name, t.teamName AS team_name, r.roleName AS role_name
             FROM user_profile up
             JOIN user_team_role utr ON up.profileId = utr.profileId
             JOIN team t ON utr.teamId = t.teamId
-            JOIN role r ON utr.roleId = r.roleId`
-    },
-    epic: {
-        createEpicQuery: 'insert into epic (epicName, epicDescription, createdById, productId) values(?,?,?,?)',
-        getEpics: 'select * from epic',
-        getEpicById: 'select * from epic WHERE epicId = ?',
-        getEpicsByProductIdQuery: `SELECT * FROM epic WHERE productId = ?`,
-        getEpicByProductId: 'select * from epic where epicId = ?',
-    },
-    sprint: {
-        createSprintQuery: 'insert into sprint (sprintName, createdById, productId, startDate, endDate) values(?,?,?,?,?)',
-        getAllSprintQuery: 'SELECT * FROM sprint',
-        getSprintByIdQuery: 'SELECT * FROM sprint WHERE sprintId = ?',
-        getSprintByProductIdQuery: `SELECT s.sprintId, s.sprintName, s.createdAt, s.startDate, s.endDate,
+            JOIN role r ON utr.roleId = r.roleId`,
+  },
+  epic: {
+    createEpicQuery:
+      "insert into epic (epicName, epicDescription, createdById, productId) values(?,?,?,?)",
+    getEpics: "select * from epic",
+    getEpicById: "select * from epic WHERE epicId = ?",
+    getEpicsByProductIdQuery: `SELECT * FROM epic WHERE productId = ?`,
+    getEpicByProductId: "select * from epic where epicId = ?",
+  },
+  sprint: {
+    createSprintQuery:
+      "insert into sprint (sprintName, createdById, productId, startDate, endDate) values(?,?,?,?,?)",
+    getAllSprintQuery: "SELECT * FROM sprint",
+    getSprintByIdQuery: "SELECT * FROM sprint WHERE sprintId = ?",
+    getSprintByProductIdQuery: `SELECT s.sprintId, s.sprintName, s.createdAt, s.startDate, s.endDate,
                 up.fname, up.lname, up.profileId,
                 p.productName, p.productId
         FROM sprint s
         JOIN user_profile up ON up.profileId = s.createdById
         JOIN product p ON s.productId = p.productId
         WHERE s.productId =?`,
-    },
-    userStory: {
-        createUserStoryQuery: 'insert into user_story (title, description, statusId, assignedToUserId, reporterUserId, userStoryPoint, productId, epicId, sprintId) values(?,?,?,?,?,?,?,?,?)',
-        createBugQuery: 'insert into user_story (title, description, statusId, assignedToUserId, reporterUserId, userStoryPoint, productId, epicId, sprintId, userStoryType, priority) values(?,?,?,?,?,?,?,?,?,?,?)',
-        getUserStoriesBySprintId: 'SELECT * FROM user_story WHERE sprintId = ?',
-        getUserStoriesByProductId: 'SELECT * FROM user_story WHERE productId = ?',
-        getUserStoriesByEpicId: 'SELECT * FROM user_story WHERE epicId = ?',
-        getDetailedUserStoriesBySprintId: `SELECT us.title, us.description, us.userStoryPoint, us.createdAt, us.userStoryType, us.priority,
+  },
+  userStory: {
+    createUserStoryQuery:
+      "insert into user_story (title, description, statusId, assignedToUserId, reporterUserId, userStoryPoint, productId, epicId, sprintId) values(?,?,?,?,?,?,?,?,?)",
+    createBugQuery:
+      "insert into user_story (title, description, statusId, assignedToUserId, reporterUserId, userStoryPoint, productId, epicId, sprintId, userStoryType, priority) values(?,?,?,?,?,?,?,?,?,?,?)",
+    getUserStoriesBySprintId: "SELECT * FROM user_story WHERE sprintId = ?",
+    getUserStoriesByProductId: "SELECT * FROM user_story WHERE productId = ?",
+    getUserStoriesByEpicId: "SELECT * FROM user_story WHERE epicId = ?",
+    getDetailedUserStoriesBySprintId: `SELECT us.title, us.description, us.userStoryPoint, us.createdAt, us.userStoryType, us.priority,
                 up.fname as assignedToFname, up.lname as assignedToLname, up.profileId as assignedtoId,
                 s.status, s.statusId,
                 p.productName, p.productId,
@@ -90,7 +111,7 @@ export const SQL_QUERIES = {
             JOIN sprint sp ON sp.sprintId = us.sprintId
             JOIN user_profile upr ON upr.profileId = us.reporterUserId
             WHERE us.sprintID = ?`,
-        getDetailedUserStoriesByProductId: `SELECT us.title, us.description, us.userStoryPoint, us.createdAt, us.userStoryType, us.priority,
+    getDetailedUserStoriesByProductId: `SELECT us.title, us.description, us.userStoryPoint, us.createdAt, us.userStoryType, us.priority,
                 up.fname as assignedToFname, up.lname as assignedToLname, up.profileId as assignedtoId, 
                 s.status, s.statusId,
                 p.productName, p.productId,
@@ -105,7 +126,7 @@ export const SQL_QUERIES = {
             JOIN sprint sp ON sp.sprintId = us.sprintId
             JOIN user_profile upr ON upr.profileId = us.reporterUserId
             WHERE us.productId = ?`,
-        getDetailedUserStoriesByEpicId: `SELECT us.title, us.description, us.userStoryPoint, us.createdAt, us.userStoryType, us.priority,
+    getDetailedUserStoriesByEpicId: `SELECT us.title, us.description, us.userStoryPoint, us.createdAt, us.userStoryType, us.priority,
                 up.fname as assignedToFname, up.lname as assignedToLname, up.profileId as assignedtoId, 
                 s.status, s.statusId,
                 p.productName, p.productId,
@@ -120,5 +141,5 @@ export const SQL_QUERIES = {
             JOIN sprint sp ON sp.sprintId = us.sprintId
             JOIN user_profile upr ON upr.profileId = us.reporterUserId
             WHERE us.epicId = ?`,
-    }
+  },
 };
