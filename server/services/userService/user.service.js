9e1e43d72825cfb1942f342c3eee4bd67c4b4d07
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-16 00:15:51 +0530

    code formatted

--- server/services/userService/user.service.js
+++ server/services/userService/user.service.js
@@ -1,57 +1,71 @@
-import {pool} from '../../config/dbConfig/database.js';
-import { encodePassword } from '../../utils/bcrypt.js'
+import { pool } from "../../config/dbConfig/database.js";
+import { encodePassword } from "../../utils/bcrypt.js";
+import { SQL_QUERIES } from "../../utils/queries.js";
 
-    async  function createUserLogin(userData) {
-        const {fname, lname, username, password} = userData;
-        const encodedPassword = encodePassword(password);
-        try{
-            const [result] = await pool.query(`insert into user_login (username, password) values(?,?)`, [username, encodedPassword]);
-            const loginId = result.insertId;
-            await createUserProfile({fname, lname, loginId});
-            return { loginId, fname, lname };
-        } catch(err) {
-            throw new Error(`Failed to create User Login: ${err.message}`);
-        };   
-    }
+async function createUserLogin(userData) {
+  const { fname, lname, username, password, role_id } = userData;
+  const encodedPassword = encodePassword(password);
+  try {
+    const [result] = await pool.query(
+      `insert into user_login (username, password) values(?,?)`,
+      [username, encodedPassword]
+    );
+    const loginId = result.insertId;
+    await createUserProfile({ fname, lname, loginId, role_id });
+    return { loginId, fname, lname };
+  } catch (err) {
+    throw new Error(`Failed to create User Login: ${err.message}`);
+  }
+}
 
-    async function createUserProfile(userData) {
-        const {fname, lname, loginId} = userData;
-        try {
-            const [result, ...meta] = await pool.query(`insert into user_profile (fname, lname, loginId) values(?,?,?)`, [fname, lname, loginId]);
-            return result.insertId;
-        } catch(err) {
-            throw new Error(`Failed to create User Profile: ${err.message}`);
-        };
-    }
+async function createUserProfile(userData) {
+  const { fname, lname, loginId, role_id } = userData;
+  try {
+    const [result, ...meta] = await pool.query(
+      `insert into user_profile (fname, lname, loginId, role_id) values(?,?,?,?)`,
+      [fname, lname, loginId, role_id]
+    );
+    return result.insertId;
+  } catch (err) {
+    throw new Error(`Failed to create User Profile: ${err.message}`);
+  }
+}
 
-    async function  updateUserProfile(userData, userId) {
-        try {
+async function updateUserProfile(userData, userId) {
+  try {
+    const query =
+      "Update user_profile SET " +
+      Object.keys(userData)
+        .map((key) => `${key} = ?`)
+        .join(",") +
+      " WHERE profileId = ?";
+    const params = [...Object.values(userData), userId];
+    const result = await pool.query(query, params);
 
-            const query = "Update user_profile SET " + Object.keys(userData).map(key => `${key} = ?`).join(',')+ " WHERE profileId = ?";
-            const params = [...Object.values(userData), userId];
-            const result = await pool.query(query, params);
-        
-            return result;
-        } catch(err) {
-            throw new Error(`Failed to update User Profile: ${err.message}`);
-        };
-    }
+    return result;
+  } catch (err) {
+    throw new Error(`Failed to update User Profile: ${err.message}`);
+  }
+}
 
-    async function  getUsers() {
-        try {
-            const [data, ...meta] = await pool.query("SELECT user_login.loginId, user_login.username, user_profile.fname, user_profile.lname FROM user_login INNER JOIN user_profile ON user_login.loginId=user_profile.loginId");
-            return data;
-        } catch(err) {
-            throw new Error(`Failed to get User detail: ${err.message}`);
-        };
-    }
+async function getUsers() {
+  try {
+    const [data, ...meta] = await pool.query(SQL_QUERIES.user.getAllUsers);
+    return data;
+  } catch (err) {
+    throw new Error(`Failed to get User detail: ${err.message}`);
+  }
+}
 
-    async function  getUserById(id) {
-        try {
-            const [data, ...meta] = await pool.query(`select * from user_profile where profileId = ?`, [id]);
-            return data;
-        } catch(err) {
-            throw new Error(`Failed to get User by id: ${err.message}`);
-        };
-    }
+async function getUserById(id) {
+  try {
+    const [data, ...meta] = await pool.query(
+      `select * from user_profile where profileId = ?`,
+      [id]
+    );
+    return data;
+  } catch (err) {
+    throw new Error(`Failed to get User by id: ${err.message}`);
+  }
+}
 export { getUsers, getUserById, createUserLogin, updateUserProfile };
