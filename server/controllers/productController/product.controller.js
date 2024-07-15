34a90c59a9785e7a6cbecaa0b87b33879ba24c04
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-16 00:15:30 +0530

    product controler/service refactored

--- server/controllers/productController/product.controller.js
+++ server/controllers/productController/product.controller.js
@@ -1,90 +1,103 @@
-import * as productService from '../../services/productService/product.service.js';
-import * as teamService from '../../services/teamService/team.service.js';
-import  * as formatter from '../../utils/formatter.js';
+import * as productService from "../../services/productService/product.service.js";
+import * as teamService from "../../services/teamService/team.service.js";
+import * as formatter from "../../utils/formatter.js";
 
-async function createProductController(req,res) {
-    const {productName, createdById, teamId} = req.body;
-    // try {
-    //     const data = await productService.createProduct(productName, createdById, teamId);
-    //     res.status(201).json(data);
-    // } catch (error) {
-    //     res.status(500).json({ error: error.message });
-    // }
-    try {
-        if(productName && createdById && teamId) {
-            // const userId = await userProfileService.saveUserProfile(fname, lname, role);
-            const data = await productService.createProduct(productName, createdById, teamId);
-            res.status(201).json(data);
-        } else {
-            res.status(500).json({ error: "productName, creator profile id and teamId is mandatory" });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+async function createProductController(req, res) {
+  const { productName, createdById, teamId } = req.body;
+  // try {
+  //     const data = await productService.createProduct(productName, createdById, teamId);
+  //     res.status(201).json(data);
+  // } catch (error) {
+  //     res.status(500).json({ error: error.message });
+  // }
+  try {
+    if (productName && createdById && !teamId) {
+      // const userId = await userProfileService.saveUserProfile(fname, lname, role);
+      const data = await productService.createProduct(productName, createdById);
+      res.status(201).json(data);
+    } else if (productName && createdById && teamId) {
+      const data = await productService.createProduct(
+        productName,
+        createdById,
+        teamId
+      );
+      res.status(201).json(data);
+    } else {
+      res
+        .status(500)
+        .json({
+          error: "productName, creator profile id and teamId is mandatory",
+        });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getAllProductsController(req,res) {
-    try {
-        const products = await productService.getProducts();
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
-
-async function getAllProductsWithTeamController(req,res) {
-    try {
-        const products = await productService.getProductsWithTeam();
-        const formattedProduct = formatter.getFormattedproduct(products);
-        res.status(200).json(formattedProduct);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
-
-async function updateProductController(req,res) {
-    const productId = req.params.id;
-    const {productName, teamId} = req.body;
-    try {
-        const success = await productService.updateProduct(productName, productId, teamId);
-        if (success) {
-            res.status(200).json({ message: 'Product updated successfully' });
-        } else {
-            res.status(404).json({ error: 'Product not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function getAllProductsController(req, res) {
+  try {
+    const products = await productService.getProducts();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
+async function getAllProductsWithTeamController(req, res) {
+  try {
+    const products = await productService.getProductsWithTeam();
+    const formattedProduct = formatter.getFormattedproduct(products);
+    res.status(200).json(formattedProduct);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
 
-
-async function getProductByIdController(req,res) {
-    const productId = req.params.id;
-    try {
-        const products = await productService.getProductById(productId);
-        const teamId = products[0]?.teamId;
-        if (products) {
-            const teamWithUsers = await teamService.getTeamWithUsersById(teamId);
-            const formattedProduct = formatter.getFormattedproduct(products);
-            const productWithTeamUsers = formatter.getFormattedproductWithTeamUsers(formattedProduct, teamWithUsers);
-            
-            res.status(200).json(productWithTeamUsers);
-        } else {
-            res.status(404).json({ error: 'Product not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+async function updateProductController(req, res) {
+  const productId = req.params.id;
+  const { productName, teamId } = req.body;
+  try {
+    const success = await productService.updateProduct(
+      productName,
+      productId,
+      teamId
+    );
+    if (success) {
+      res.status(200).json({ message: "Product updated successfully" });
+    } else {
+      res.status(404).json({ error: "Product not found" });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function getProductByIdController(req, res) {
+  const productId = req.params.id;
+  try {
+    const products = await productService.getProductById(productId);
+    const teamId = products[0]?.teamId;
+    if (products) {
+      const teamWithUsers = await teamService.getTeamWithUsersById(teamId);
+      const formattedProduct = formatter.getFormattedproduct(products);
+      const productWithTeamUsers = formatter.getFormattedproductWithTeamUsers(
+        formattedProduct,
+        teamWithUsers
+      );
+
+      res.status(200).json(productWithTeamUsers);
+    } else {
+      res.status(404).json({ error: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export {
+  createProductController,
+  updateProductController,
+  getAllProductsController,
+  getAllProductsWithTeamController,
+  getProductByIdController,
 };
-
-export { 
-    createProductController,
-    updateProductController,
-    getAllProductsController,
-    getAllProductsWithTeamController,
-    getProductByIdController,
-};
-
-
-
--- server/services/productService/product.service.js
+++ server/services/productService/product.service.js
@@ -1,71 +1,112 @@
-import {pool} from '../../config/dbConfig/database.js';
-import {SQL_QUERIES} from '../../utils/queries.js';
+import { pool } from "../../config/dbConfig/database.js";
+import { SQL_QUERIES } from "../../utils/queries.js";
 
 async function createProduct(productName, profileId, teamId) {
-    try{
-        let query;
-        let values;
-        if(teamId) {
-            query = SQL_QUERIES.product.insertWithTeamQuery;
-            values = [productName, profileId, teamId];
-        } else {
-            query = SQL_QUERIES.product.insertWithoutTeamQuery;
-            values = [productName, profileId];
-        }
-        const result = await pool.query(query, values);
-        return result[0];
-    } catch(err) {
-        throw new Error(`Failed to create product: ${err.message}`);
-    };
-    
+  try {
+    let query;
+    let values;
+    if (teamId) {
+      console.log("teamId >> ", teamId);
+      query = SQL_QUERIES.product.insertWithTeamQuery;
+      values = [productName, profileId, teamId, profileId];
+    } else {
+      console.log("teamId >> ", teamId);
+      query = SQL_QUERIES.product.insertWithoutTeamQuery;
+      values = [productName, profileId, profileId];
+    }
+    const result = await pool.query(query, values);
+    return result[0];
+  } catch (err) {
+    throw new Error(`Failed to create product: ${err.message}`);
+  }
 }
 
-async function  getProducts() {
-    try {
-        const [data, ...meta] = await pool.query(SQL_QUERIES.product.getProductsQuery);
-        return data;
-    } catch(err) {
-        throw new Error(`Failed to get products: ${err.message}`);
-    };
+async function getProducts() {
+  try {
+    const [data, ...meta] = await pool.query(
+      SQL_QUERIES.product.getProductsQuery
+    );
+    return data;
+  } catch (err) {
+    throw new Error(`Failed to get products: ${err.message}`);
+  }
 }
 
-async function  getProductById(id) {
-    try {
-        const [data, ...meta] = await pool.query(SQL_QUERIES.product.getProductByIdQuery, [id]);
-        return data;
-    } catch(err) {
-        throw new Error(`Failed to get product by id: ${err.message}`);
-    };
+async function getProductById(id) {
+  try {
+    const [data, ...meta] = await pool.query(
+      SQL_QUERIES.product.getProductByIdQuery,
+      [id]
+    );
+    return data;
+  } catch (err) {
+    throw new Error(`Failed to get product by id: ${err.message}`);
+  }
 }
 
-async function  updateProduct(productName, productId, teamId) {
-    try {
-        let query, values;
-        if(!teamId && productName) {
-            query = SQL_QUERIES.product.updateProductName;
-            values = [productName, productId];
-        } else if(teamId && !productName) {
-            query = SQL_QUERIES.product.updateProductTeam;
-            values = [teamId, productId];
-        } else {
-            query = SQL_QUERIES.product.updateProductNameAndTeam;
-            values = [productName, productId , teamId];
-        }
-        const result = await pool.query(query, values);
-        return result;
-    } catch(err) {
-        throw new Error(`Failed to update product: ${err.message}`);
-    };
+// async function updateProduct(productName, productId, teamId) {
+//   try {
+//     let query, values;
+//     if (!teamId && productName) {
+//       query = SQL_QUERIES.product.updateProductName;
+//       values = [productName, productId];
+//     } else if (teamId && !productName) {
+//       query = SQL_QUERIES.product.updateProductTeam;
+//       values = [teamId, productId];
+//     } else {
+//       query = SQL_QUERIES.product.updateProductNameAndTeam;
+//       values = [productName, productId, teamId];
+//     }
+//     const result = await pool.query(query, values);
+//     return result;
+//   } catch (err) {
+//     throw new Error(`Failed to update product: ${err.message}`);
+//   }
+// }
+
+async function updateProduct(
+  productName,
+  productId,
+  teamId,
+  product_owner_id = null
+) {
+  try {
+    let query, values;
+    if (!teamId && !product_owner_id && productName) {
+      query = SQL_QUERIES.product.updateProductName;
+      values = [productName, productId];
+    } else if (teamId && !productName && !product_owner_id) {
+      query = SQL_QUERIES.product.updateProductTeam;
+      values = [teamId, productId];
+    } else if (!teamId && !productName && product_owner_id) {
+      query = SQL_QUERIES.product.updateProductOwner;
+      values = [product_owner_id, productId];
+    } else {
+      query = SQL_QUERIES.product.updateProductNameAndTeam;
+      values = [productName, teamId, product_owner_id, productId];
+    }
+    const result = await pool.query(query, values);
+    return result;
+  } catch (err) {
+    throw new Error(`Failed to update product: ${err.message}`);
+  }
 }
 
-async function  getProductsWithTeam() {
-    try {
-        const [data, ...meta] = await pool.query(SQL_QUERIES.product.getProductsWithTeamQuery);
-        return data;
-    } catch(err) {
-        throw new Error(`Failed to get products: ${err.message}`);
-    };
+async function getProductsWithTeam() {
+  try {
+    const [data, ...meta] = await pool.query(
+      SQL_QUERIES.product.getProductsWithTeamQuery
+    );
+    return data;
+  } catch (err) {
+    throw new Error(`Failed to get products: ${err.message}`);
+  }
 }
 
-
-export { createProduct, getProducts, getProductById, updateProduct, getProductsWithTeam };
+export {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  getProductsWithTeam,
+};
