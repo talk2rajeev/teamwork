4c35459c82cfc54caf19564ae77fb684487a563f
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-18 00:03:43 +0530

    logged-in user logout dropdown

--- client-react-redux/src/components/appComponents/pageHeader/pageHeader.tsx
+++ client-react-redux/src/components/appComponents/pageHeader/pageHeader.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import Navigation from '../navigation/Navigation';
 import { Logo } from '../logo/Logo';
+import { AuthUtil } from '../../../utils/auth/auth';
+import Popover from '../../popover/popover';
 
 function PageHeader() {
+  const userDetail = AuthUtil.getUserDetail();
   const handleMenuItemClick = (
     event: React.MouseEvent<HTMLSpanElement, MouseEvent>
   ) => {
     const target = event.target as HTMLSpanElement;
     console.log('menu clicked ', target.dataset.item);
   };
+
+  const settingOnclick = (
+    e: React.MouseEvent<HTMLParagraphElement, MouseEvent>
+  ) => {
+    const targetElement = e.currentTarget as HTMLParagraphElement;
+    console.log('data ', targetElement.dataset);
+  };
+
+  const Settings = (
+    <div>
+      <p data-item="settings" onClick={settingOnclick}>
+        Settings
+      </p>
+      <p data-item="logout" onClick={settingOnclick}>
+        Logout
+      </p>
+    </div>
+  );
+
   return (
     <header className="grid grid-cols-3 gap-4">
       <Logo />
       <div className="col-span-2 justify-self-end">
         <div className="grid grid-flow-col auto-cols-max gap-6 pt-2">
-          {/* <MenuLink onclick={handleMenuItemClick} label='Dashboard' active={true}/> 
-                    <MenuLink onclick={handleMenuItemClick} label='Projects'/> 
-                    <MenuLink onclick={handleMenuItemClick} label='Sprints'/> 
-                    <MenuLink onclick={handleMenuItemClick} label='Backlogs'/> 
-                    <MenuLink onclick={handleMenuItemClick} label='Epics'/>  */}
           <Navigation />
+          <Popover content={Settings}>
+            {userDetail?.fname} {userDetail?.lname}
+          </Popover>
         </div>
       </div>
     </header>
