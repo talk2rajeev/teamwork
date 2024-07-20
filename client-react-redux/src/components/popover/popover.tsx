8faa12874b32965aca54096c35cb8b270072f421
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-20 10:10:25 +0530

    link or button type

--- client-react-redux/src/components/popover/popover.tsx
+++ client-react-redux/src/components/popover/popover.tsx
@@ -5,6 +5,8 @@ interface PopoverProps {
   children: React.ReactNode;
   type?: 'warning' | 'error' | 'primary' | 'default';
   title?: string;
+  linkType?: 'button' | 'link';
+  position?: string;
 }
 
 const Popover: FC<PopoverProps> = ({
@@ -12,6 +14,8 @@ const Popover: FC<PopoverProps> = ({
   children,
   type = 'default',
   title,
+  linkType = 'link',
+  position,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const popoverRef = useRef<HTMLDivElement>(null);
@@ -36,14 +40,24 @@ const Popover: FC<PopoverProps> = ({
 
   return (
     <div className="relative inline-block" ref={popoverRef}>
-      <button
-        onClick={togglePopover}
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-      >
-        {children}
-      </button>
+      {linkType === 'button' && (
+        <button
+          onClick={togglePopover}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+        >
+          {children}
+        </button>
+      )}
+      {linkType === 'link' && (
+        <span
+          onClick={togglePopover}
+          className=" text-slate-700 rounded hover:text-slate-600"
+        >
+          {children}
+        </span>
+      )}
       {isOpen && (
-        <div className="absolute z-10 mt-2 w-48">
+        <div className={`absolute z-10 mt-2 w-48 ${position}`}>
           <div className="relative bg-white border border-gray-200 rounded shadow-lg">
             <div className="absolute -top-2 left-1/2 transform -translate-x-1/2">
               <div className="w-0 h-0 border-l-8 border-r-8 border-b-8 border-transparent border-b-white"></div>
