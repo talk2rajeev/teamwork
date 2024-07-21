053085d5300b9e4b7ff51059c68fb87056f735b0
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-21 07:13:44 +0530

    product reducer added to Store

--- client-react-redux/src/appStore/store.ts
+++ client-react-redux/src/appStore/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import loginReducer from '../pages/login/loginSlice';
+import productReducer from '../slices/product/productSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     login: loginReducer,
+    product: productReducer,
   },
 });
 
