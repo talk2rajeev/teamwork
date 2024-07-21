98766755d8d3d6c9aa592ccd6f11b216d9f26896
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-21 07:12:26 +0530

    display  product list, code refactored

--- client-react-redux/src/pages/dashboard/Dashboard.tsx
+++ client-react-redux/src/pages/dashboard/Dashboard.tsx
@@ -1,43 +1,57 @@
 import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import Layout from '../../components/layout/Layout';
-import CreateStory from '../../components/appComponents/createStory/CreateStory';
-import Modal from '../../components/modal/Modal';
-import Popover from '../../components/popover/popover';
 import * as coreComponents from '../../components/core-components';
 import { AuthUtil } from '../../utils/auth/auth';
 
 const Dashboard: React.FC = () => {
-  const [isModalOpen, setModalOpen] = useState<boolean>(false);
-  const openModal = () => setModalOpen(true);
-  const closeModal = () => setModalOpen(false);
-  console.log('getUesrDetaul ', AuthUtil.getUserDetail());
+  const params = useParams<{ productId: string }>();
+  const location = useLocation();
+
+  // console.log('getUesrDetaul ', AuthUtil.getUserDetail());
+  // console.log('params >>>>>>>>>>>>> ', params);
+  console.log('location ;;;;; ', location);
+
+  const isDashboardRootPage = location.pathname === '/dashboard';
+
+  const isActive = ({ isActive }: { isActive: boolean }) =>
+    isActive
+      ? 'inline-block  text-blue-500 hover:text-blue-600 active:text-blue-700 p-2 border-b-2 border-blue-500'
+      : 'inline-block  text-slate-500 hover:text-blue-600 active:text-blue-700 p-2';
 
   return (
     <Layout>
-      <h2>Dashboard Page</h2>
-      <div className="mb-4 mt-4">
-        <Popover
-          title="Popover Header"
-          content={<p>This is the popover content.</p>}
-        >
-          Click
-        </Popover>
-      </div>
-
-      <coreComponents.Button
-        label="Create Story"
-        type="primary"
-        clickHandler={openModal}
-      />
-      <Modal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        title="Create User Story"
-        size="lg"
-        footer={true}
-      >
-        <CreateStory />
-      </Modal>
+      {isDashboardRootPage ? (
+        <div>Dashboard Root Page</div>
+      ) : (
+        <div>
+          <div className="grid grid-flow-col auto-cols-max gap-8">
+            <div className="">
+              <NavLink to="product" className={isActive}>
+                Product
+              </NavLink>
+            </div>
+            <div className="">
+              <NavLink to="team" className={isActive}>
+                Team
+              </NavLink>
+            </div>
+            <div className="">
+              <NavLink to="users" className={isActive}>
+                User
+              </NavLink>
+            </div>
+          </div>
+          <div
+            className="border-t-1 border-slate-400 border-solid"
+            style={{ marginTop: '-0.10rem' }}
+          >
+            <Outlet />
+          </div>
+        </div>
+      )}
     </Layout>
   );
 };
