f8618cf9d69391ca1d9a4a69d97768ed8e7879eb
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-23 21:56:19 +0530

    createStory component used here

--- client-react-redux/src/pages/sprintBoard/SprintBoard.tsx
+++ client-react-redux/src/pages/sprintBoard/SprintBoard.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Layout from '../../components/layout/Layout';
+import CreateStory from '../../components/appComponents/createStory/CreateStory';
+import Modal from '../../components/modal/Modal';
+import * as coreComponents from '../../components/core-components';
+import Popover from '../../components/popover/popover';
 
 const SprintBoard: React.FC = () => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
+  const params = useParams<{ productId: string }>();
+
+  console.log('params >>>>>>>>>>>>> ', params);
+
   const { productId } = useParams<{ productId: string }>();
   console.log(productId);
   return (
     <Layout>
-      <h2>SprintBoard Page</h2>
+      <div className="mb-4 mt-4">
+        <Popover
+          title="Change Product"
+          content={
+            <coreComponents.SearchableCombobox
+              items={['Epirurian', 'Next Gen', 'OMDS', 'Managed Extensions']}
+              onSelect={(item: string) => {
+                console.log(item);
+              }}
+              label="Product Name"
+              placeholder="Search Product"
+            />
+          }
+        >
+          Managed Extensions
+        </Popover>
+      </div>
+
+      <coreComponents.Button
+        label="Create Story"
+        type="primary"
+        clickHandler={openModal}
+      />
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        title="Create User Story"
+        size="lg"
+        footer={false}
+      >
+        <CreateStory />
+      </Modal>
     </Layout>
   );
 };
