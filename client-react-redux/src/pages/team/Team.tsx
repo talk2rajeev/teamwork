a1c8c7fe30c7c97be723e0bedf9e818bdd3ebd06
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-07-24 21:00:53 +0530

    Team component created

--- client-react-redux/src/pages/team/Team.tsx
+++ client-react-redux/src/pages/team/Team.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { AuthUtil } from '../../utils/auth/auth';
 
 const Team: React.FC = () => {
+  const userDetail = AuthUtil.getUserDetail();
+  const isAdmin = userDetail?.roleId && userDetail?.roleId === 1;
   return (
     <div>
+      {isAdmin && <div>Create Team</div>}
       <h2>Team list</h2>
     </div>
   );
