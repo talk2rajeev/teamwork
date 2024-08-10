d274aa584f01a10937076aae6ff49801914c0616
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-08-10 11:09:16 +0530

    team reducer added

--- client-react-redux/src/appStore/store.ts
+++ client-react-redux/src/appStore/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import loginReducer from '../slices/login/loginSlice';
 import productReducer from '../slices/product/productSlice';
+import teamReducer from '../slices/team/teamSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     login: loginReducer,
     product: productReducer,
+    team: teamReducer,
   },
 });
 
