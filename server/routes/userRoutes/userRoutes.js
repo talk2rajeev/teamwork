f806bee3add7ae0b01a55cb4b350cf3540e8efd7
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-08-13 00:43:24 +0530

    getAdminUsers route added

--- server/routes/userRoutes/userRoutes.js
+++ server/routes/userRoutes/userRoutes.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import * as userControllers from '../../controllers/userController/user.controller.js';
+import express from "express";
+import * as userControllers from "../../controllers/userController/user.controller.js";
 
 const userRoutes = express.Router();
 
-
-userRoutes.post('/createLogin', userControllers.createUserLoginController);
-userRoutes.post('/updateProfile/:id', userControllers.updateUserProfileController);
-userRoutes.get('/getAllUsers', userControllers.getUsersController);
-userRoutes.get('/getUserById/:id', userControllers.getUserByIdController);
+userRoutes.post("/createLogin", userControllers.createUserLoginController);
+userRoutes.post(
+  "/updateProfile/:id",
+  userControllers.updateUserProfileController
+);
+userRoutes.get("/getAllUsers", userControllers.getUsersController);
+userRoutes.get("/getUserById/:id", userControllers.getUserByIdController);
+userRoutes.get("/getAdminUsers", userControllers.getAdminUsersController);
 
 export { userRoutes };
-
-
-
