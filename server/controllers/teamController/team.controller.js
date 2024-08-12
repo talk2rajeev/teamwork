48bdea96464855625cc1ed53146c8ed983708ae0
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-08-13 00:41:19 +0530

    code preetier

--- server/controllers/teamController/team.controller.js
+++ server/controllers/teamController/team.controller.js
@@ -1,120 +1,111 @@
-import * as teamService from '../../services/teamService/team.service.js';
+import * as teamService from "../../services/teamService/team.service.js";
+import { getFormattedTeamsWithUsers } from "../../utils/formatter.js";
 
-
-async function createTeamController(req,res) {
-    const {teamName, createdById} = req.body;
-    try {
-        // const userId = await userProfileService.saveUserProfile(fname, lname, role);
-        const data = await teamService.createTeam(teamName, createdById);
-        res.status(201).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function createTeamController(req, res) {
+  const { teamName, createdById } = req.body;
+  try {
+    // const userId = await userProfileService.saveUserProfile(fname, lname, role);
+    const data = await teamService.createTeam(teamName, createdById);
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function updateTeamController(req,res) {
-    const {teamName} = req.body;
-    const teamId = req.params.id;
-    try {
-        const success = await teamService.updateTeam(teamName, teamId);
-        if (success) {
-            res.status(200).json({ message: 'Team updated successfully' });
-        } else {
-            res.status(404).json({ error: 'Team not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+async function updateTeamController(req, res) {
+  const { teamName } = req.body;
+  const teamId = req.params.id;
+  try {
+    const success = await teamService.updateTeam(teamName, teamId);
+    if (success) {
+      res.status(200).json({ message: "Team updated successfully" });
+    } else {
+      res.status(404).json({ error: "Team not found" });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getTeamsController(req,res) {
-    try {
-        const users = await teamService.getTeams();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
+async function getTeamsController(req, res) {
+  try {
+    const users = await teamService.getTeams();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
 
-async function getTeamByIdController(req,res) {
-    const id = req.params.id;
-    try {
-        const user = await teamService.getTeamById(id);
-        if (user) {
-            res.status(200).json(user);
-        } else {
-            // If user is not found, return 404
-            res.status(404).json({ error: 'Team not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+async function getTeamByIdController(req, res) {
+  const id = req.params.id;
+  try {
+    const user = await teamService.getTeamById(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      // If user is not found, return 404
+      res.status(404).json({ error: "Team not found" });
     }
-};
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
 
-async function getTeamWithUsersByIdController(req,res) {
-    const id = req.params.id;
-    try {
-        const user = await teamService.getTeamWithUsersById(id);
-        if (user) {
-            res.status(200).json(user);
-        } else {
-            res.status(404).json({ error: 'Team not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+async function getTeamWithUsersByIdController(req, res) {
+  const id = req.params.id;
+  try {
+    const user = await teamService.getTeamWithUsersById(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: "Team not found" });
     }
-};
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
 
 async function assignUserRoleInTeamController(req, res) {
-    const {teamId, profileId, roleId} = req.body;
-    try {
-        const data = await teamService.assignUserRoleInTeam(teamId, profileId, roleId);
-        res.status(201).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  const { teamId, profileId, roleId } = req.body;
+  try {
+    const data = await teamService.assignUserRoleInTeam(
+      teamId,
+      profileId,
+      roleId
+    );
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 async function updateUserRoleInTeamController(req, res) {
- const {roleId, userTeamRoleId} = req.body;
-    try {
-        const data = await teamService.updateUserRoleInTeam(roleId, userTeamRoleId);
-        res.status(201).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  const { roleId, userTeamRoleId } = req.body;
+  try {
+    const data = await teamService.updateUserRoleInTeam(roleId, userTeamRoleId);
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getAllTeamsWithUsersController(req,res) {
-    try {
-        const teamWithUsers = await teamService.getAllTeamsWithUsers();
-        
-        const map = {};
-        teamWithUsers.forEach(t => {
-            const teamUsers = map[t.team_name];
-            console.log(teamUsers);
-            if(teamUsers) {
-                teamUsers.push({first_name: t.first_name, last_name: t.last_name, role_name: t.role_name})
-            } else {
-                map[t.team_name] = [{first_name: t.first_name, last_name: t.last_name, role_name: t.role_name}]
-            }
-        });
-        res.status(200).json(map);
-    } catch (error) {
-        res.status(500).json({ error: error.message });s
-    }
+async function getAllTeamsWithUsersController(req, res) {
+  try {
+    const teamWithUsers = await teamService.getAllTeamsWithUsers();
+    var formattedTeamsWithUsers = getFormattedTeamsWithUsers(teamWithUsers);
+    res.status(200).json(formattedTeamsWithUsers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export {
+  createTeamController,
+  updateTeamController,
+  getTeamsController,
+  getTeamByIdController,
+  getTeamWithUsersByIdController,
+  assignUserRoleInTeamController,
+  updateUserRoleInTeamController,
+  getAllTeamsWithUsersController,
 };
-
-export { 
-    createTeamController,
-    updateTeamController,
-    getTeamsController,
-    getTeamByIdController,
-    getTeamWithUsersByIdController,
-    assignUserRoleInTeamController,
-    updateUserRoleInTeamController,
-    getAllTeamsWithUsersController
-};
-
-
-
