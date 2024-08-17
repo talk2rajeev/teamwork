abb8ee5266b5c32c24b1185fbb9eb7556d83d20c
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-08-18 01:04:59 +0530

    antdesign notification used for api suceess/fail

--- client-react-redux/src/components/appComponents/updateProduct/UpdateProduct.tsx
+++ client-react-redux/src/components/appComponents/updateProduct/UpdateProduct.tsx
@@ -1,4 +1,4 @@
-import react, { useState } from 'react';
+import react, { useState, useEffect } from 'react';
 import { ImSpinner5 } from 'react-icons/im';
 import * as coreComponents from '../../core-components';
 import { useAppSelector, useAppDispatch } from '../../../appStore/hooks';
@@ -6,57 +6,76 @@ import * as Types from '../../../utils/types/types';
 import { getProductWithTeam } from '../../../utils/helperFunctions/product/productHelper';
 import { teamWithUsers } from '../../../slices/team/teamSlice';
 import { allUsers } from '../../../slices/users/userSlice';
+import { useDebounce } from '../../../hooks/debounce/debounce';
+import {
+  setProductId,
+  setProductName,
+  setProductTeamId,
+  setProductOwnerId,
+  selectedProductId,
+  updateProductFormData,
+} from '../../../slices/product/productSlice';
+import { GiConfirmed } from 'react-icons/gi';
+import { MdError } from 'react-icons/md';
 
 type UpdateProductProps = {
-  inputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   type: Types.ActionType;
   selectedProduct?: Types.SelectedProduct;
   product?: Types.Product;
 };
 
-type FormDataType = {
-  productName?: string;
-  product_owner_id?: number;
-  teamId?: number;
-};
-
 const UpdateProduct: React.FC<UpdateProductProps> = ({
-  inputChange,
   type,
   selectedProduct,
   product,
 }) => {
-  // Featch team list & create a listOption out of it for team combobox
-  // Fetch UserProfileList & create a listOption out of it for productOwner combobox
+  // on update success:: handle it better UX way
+  // on update failure : handle error
 
+  const dispatch = useAppDispatch();
   const teams = useAppSelector(teamWithUsers);
   const users = useAppSelector(allUsers);
+  const productId = useAppSelector(selectedProductId);
+  const formData = useAppSelector(updateProductFormData);
   const productWithTeam = getProductWithTeam(selectedProduct, product);
   const userList = users.users.map((u) => `${u.fname} ${u.lname}`);
+  const [prodName, setProdName] = useState<string>('');
 
-  const [formData, setFormData] = useState<FormDataType>();
-
-  const debouncedInputChange = () => {};
+  const disabled = type === 'view';
 
   const onInputChange = (
     event: React.ChangeEvent<HTMLInputElement>,
     data?: any
   ) => {
-    console.log(event.target.name, event.target.value);
-    setFormData({ ...formData, productName: '' });
-    inputChange(event);
+    setProdName(event.target.value);
   };
 
+  const debouncedProductName = useDebounce(prodName, 500);
+
+  useEffect(() => {
+    if (debouncedProductName) {
+      dispatch(setProductName(debouncedProductName));
+    }
+  }, [debouncedProductName]);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(setProductId(productId));
+    }
+  }, [productId]);
+
   const onTeamSelect = (item: string) => {
-    console.log(item);
     const team_id = teams.find((t) => t.team_name === item)?.team_id;
-    if (team_id) setFormData({ ...formData, teamId: team_id });
+    if (team_id) {
+      dispatch(setProductTeamId(team_id));
+    }
   };
 
   const onProdOwnerSelect = (item: string) => {
-    console.log(item);
     const user = users.users.find((u) => `${u.fname} ${u.lname}` === item);
-    if (user) setFormData({ ...formData, product_owner_id: user.profileId });
+    if (user) {
+      dispatch(setProductOwnerId(user.profileId));
+    }
   };
 
   const teamOption = teams.map((t) => t.team_name);
@@ -69,8 +88,34 @@ const UpdateProduct: React.FC<UpdateProductProps> = ({
     );
   }
 
+  const productName = prodName || productWithTeam.productName;
+
+  //   const updateSucceded =
+  //     formData.apiResponseStatus !== '' &&
+  //     formData.apiResponseStatus === 'success';
+  //   const updateFailed =
+  //     formData.apiResponseStatus !== '' &&
+  //     formData.apiResponseStatus === 'failed';
+
+  //   if (updateSucceded) {
+  //     return (
+  //       <div>
+  //         <GiConfirmed fill="#5cb85c" size="24" />
+  //         <span>{formData.message}</span>
+  //       </div>
+  //     );
+  //   }
+
+  //   if (updateFailed) {
+  //     return (
+  //       <div>
+  //         <MdError fill="#5cb85c" size="16" />
+  //         <span>{formData.message}</span>
+  //       </div>
+  //     );
+  //   }
   return (
-    <div className="user-story-container bg-white p-2">
+    <div className="user-story-container bg-white p-2 min-h-96 grid grid-cols-1 content-between">
       <div>
         <div className="p-2">
           <coreComponents.Input
@@ -79,8 +124,9 @@ const UpdateProduct: React.FC<UpdateProductProps> = ({
             name="productName"
             placeholder="Product Name"
             onchange={onInputChange}
-            value={productWithTeam.productName}
+            value={productName}
             classes="text-base bg-white border-1 border-slate-300 outline-slate-400"
+            disabled={disabled}
           />
         </div>
         <div className="p-2">
@@ -89,6 +135,7 @@ const UpdateProduct: React.FC<UpdateProductProps> = ({
             onSelect={onTeamSelect}
             label="Team"
             value={productWithTeam.productTeam}
+            disabled={disabled}
           />
         </div>
         <div className="p-2">
@@ -97,6 +144,23 @@ const UpdateProduct: React.FC<UpdateProductProps> = ({
             onSelect={onProdOwnerSelect}
             label="Product Owner"
             value={productWithTeam.productOwnerName}
+            disabled={disabled}
+          />
+        </div>
+      </div>
+      <div className="grid grid-flow-col auto-cols-max justify-end">
+        <div className="p-2">
+          <coreComponents.Button
+            label="Login"
+            type="primary"
+            clickHandler={() => {}}
+          />
+        </div>
+        <div className="p-2">
+          <coreComponents.Button
+            label="No Login"
+            type="default"
+            clickHandler={() => {}}
           />
         </div>
       </div>
