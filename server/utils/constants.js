4104b5d0458fee0a8597e9383b1a29594bdbbccf
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-10-25 23:45:54 +0530

    responseData constant for generic response

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -1,7 +1,14 @@
 function minutesToSeconds(min) {
-    return 60 * min;
+  return 60 * min;
 }
 
 const TOKEN_EXPIRE_TIME = 360;
 
 export const EXPIRE_TIME = minutesToSeconds(TOKEN_EXPIRE_TIME);
+
+export const responseData = {
+  success: false,
+  status: 400,
+  message: "",
+  data: null,
+};
