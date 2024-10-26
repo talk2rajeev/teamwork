c9cb307632762432db45b764ed4e8d97eb38a019
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-10-26 14:23:49 +0530

    Typescript support for createAsyncThunk

--- client-react-redux/src/slices/team/teamSlice.ts
+++ client-react-redux/src/slices/team/teamSlice.ts
@@ -2,38 +2,9 @@ import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState, AppThunk } from '../../appStore/store';
 import * as fetcher from '../../services/fetcher/fetcher';
 import * as Types from '../../utils/types/types';
-// import { login, logoutAsyncApi } from './loginAPI';
+import { showNotification } from '../notificationSlice/notificationSlice';
 
-// type productWithTeam = product & {
-//   team: { teamId: number; teamName: string };
-// };
-
-type status = 'idle' | 'loading' | 'failed';
-type TeamCreated = {
-  message?: string;
-  error?: boolean;
-  errorMessage?: string;
-  status: status;
-};
-
-export interface TeamState {
-  TeamWithUser: {
-    status: Types.StatusType;
-    TeamWithUsers: Types.TeamWithUserInterface[];
-  };
-  allTeams: {
-    status: Types.StatusType;
-    teams: Array<Types.Team>;
-  };
-  selectedTeamId: number;
-  teamCreated?: TeamCreated;
-  updateTeam: {
-    status: 'idle' | 'loading' | 'failed' | 'success';
-    message?: string;
-  };
-}
-
-const initialState: TeamState = {
+const initialState: Types.TeamState = {
   TeamWithUser: {
     status: 'idle',
     TeamWithUsers: [],
@@ -48,6 +19,11 @@ const initialState: TeamState = {
     status: 'idle',
     message: undefined,
   },
+  deleteTeamUser: {
+    status: 'idle',
+    message: undefined,
+  },
+  todo: '',
 };
 
 // The function below is called a thunk and allows us to perform async logic. It
@@ -55,84 +31,132 @@ const initialState: TeamState = {
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched. Thunks are
 // typically used to make async requests.
-export const createTeamAsync = createAsyncThunk(
-  'team/createTeam',
-  async (reqPayload: { teamName: string; createdById: number }) => {
-    const response = await fetcher.post<Types.CreateResponseType>(
-      '/team/createTeam',
-      reqPayload
-    );
-    // The value we return becomes the `fulfilled` action payload
-    return response;
-  }
-);
+type EmptyReqPayloadType = {};
+type ReqPayloadType = { teamName: string; createdById: number };
 
-export const getAllTeams = createAsyncThunk('/team/getAllTeams', async () => {
-  type TeamType = {
-    createdByProfileId: number;
-    createdByFname: string;
-    createdByLname: string;
-    teamId: number;
-    teamName: string;
-  };
-  const response = await fetcher.get<Array<TeamType>>('/team/getAllTeams');
+export const createTeamAsync = createAsyncThunk<
+  Types.GenericResponseType<undefined>,
+  ReqPayloadType
+>('team/createTeam', async (reqPayload: ReqPayloadType, thunkAPI) => {
+  const response = await fetcher.post<Types.GenericResponseType<undefined>>(
+    '/team/createTeam',
+    { teamNames: reqPayload.teamName, createdById: reqPayload.createdById }
+  );
   // The value we return becomes the `fulfilled` action payload
   return response;
 });
 
-export const getTeamsWithUserByTeamId = createAsyncThunk(
-  '/team/getTeamWithUsersById',
-  async (teamId: string, { dispatch, getState }) => {
-    const state = getState() as RootState;
-    const response = await fetcher.get<Array<Types.TeamWithUserInterface>>(
-      `/team/getTeamWithUsersById/${teamId}`
+export const getAllTeams = createAsyncThunk<
+  Types.GenericResponseType<Types.TeamType[]>
+>('/team/getAllTeams', async () => {
+  const response =
+    await fetcher.get<Types.GenericResponseType<Types.TeamType[]>>(
+      '/team/getAllTeams'
     );
-    let users: Types.TeamUser[] = [];
-    if (response.length) {
-      users = response[0].users;
-    } else {
-      const selectedTeam = state.team.allTeams.teams.find(
-        (t) => t.team_id === Number(teamId)
-      );
-      if (selectedTeam) users = selectedTeam.users || [];
-    }
-    return { teamId: Number(teamId), users };
-  }
-);
+  // The value we return becomes the `fulfilled` action payload
 
-export const getTeamsWithUsersAsync = createAsyncThunk(
-  '/team/getTeamsWithUsers',
-  async () => {
-    const response = await fetcher.get<Array<Types.TeamWithUserInterface>>(
-      '/team/getAllTeamsWithUsers'
-    );
-    // The value we return becomes the `fulfilled` action payload
-    return response;
-  }
-);
+  return response;
+});
 
-export const updateTeamNameAsync = createAsyncThunk(
-  '/team/updateTeamName',
-  async (teamObj: { teamId: number; teamName: string }, thunkAPI) => {
-    const reqPayload = { teamName: teamObj.teamName };
-    const response = await fetcher.put<{ message: string }>(
-      `/team/updateTeam/${teamObj.teamId}`,
-      reqPayload
-    );
-    thunkAPI.dispatch(setTeamName(teamObj));
-    // The value we return becomes the `fulfilled` action payload
-    return response;
-  }
-);
+type TeamWithUserByIdReturnType = {
+  teamId: number;
+  users: Array<Types.TeamUser>;
+};
+export const getTeamsWithUserByTeamId = createAsyncThunk<
+  TeamWithUserByIdReturnType,
+  string
+>('/team/getTeamWithUsersById', async (teamId: string, thunkAPI) => {
+  const state = thunkAPI.getState() as RootState;
+  const response = await fetcher.get<Types.GenericResponseType<Types.Team[]>>(
+    `/team/getTeamWithUsersById/${teamId}`
+  );
 
-export const assignUserRoleInTeamAsync = createAsyncThunk(
+  const returnData: TeamWithUserByIdReturnType = {
+    teamId: Number(teamId),
+    users: [],
+  };
+  if (response.data.length && response.data[0].users) {
+    returnData.users = response.data[0].users;
+  }
+
+  return returnData;
+});
+
+export const getTeamsWithUsersAsync = createAsyncThunk<
+  Array<Types.TeamWithUserInterface>
+>('/team/getTeamsWithUsers', async () => {
+  const response = await fetcher.get<Array<Types.TeamWithUserInterface>>(
+    '/team/getAllTeamsWithUsers'
+  );
+  // The value we return becomes the `fulfilled` action payload
+  return response;
+});
+
+type UpdateTeamReqPayload = { teamId: number; teamName: string };
+export const updateTeamNameAsync = createAsyncThunk<
+  Types.GenericResponseType<Types.Team>,
+  UpdateTeamReqPayload
+>('/team/updateTeamName', async (teamObj: UpdateTeamReqPayload, thunkAPI) => {
+  const reqPayload = { teamName: teamObj.teamName };
+  const response = await fetcher.put<Types.GenericResponseType<Types.Team>>(
+    `/team/updateTeam/${teamObj.teamId}`,
+    reqPayload
+  );
+  thunkAPI.dispatch(setTeamName(teamObj));
+  // The value we return becomes the `fulfilled` action payload
+  return response;
+});
+
+export const assignUserRoleInTeamAsync = createAsyncThunk<
+  Types.GenericResponseType<undefined>,
+  Types.AssignTeamUserReqPayload
+>(
   '/team/assignUserRoleInTeam',
   async (reqPayload: Types.AssignTeamUserReqPayload, thunkAPI) => {
-    const response = await fetcher.post<{ message: string }>(
-      `/team/assignUserRoleInTeam}`,
+    const response = await fetcher.post<Types.GenericResponseType<undefined>>(
+      `/team/assignUserRoleInTeam`,
       reqPayload
     );
-    thunkAPI.dispatch(getTeamsWithUserByTeamId(reqPayload.teamId.toString()));
+    // if (response.affectedRows === 1) {
+    //   thunkAPI.dispatch(
+    //     showNotification({
+    //       type: 'success',
+    //       title: 'Success.',
+    //       message: 'Product created successfully!',
+    //     })
+    //   );
+    // } else {
+    //   thunkAPI.dispatch(
+    //     showNotification({
+    //       type: 'success',
+    //       title: 'Failed!',
+    //       message: 'Failed to assign user to Team.',
+    //     })
+    //   );
+    // }
+
+    if (response.success)
+      thunkAPI.dispatch(getTeamsWithUserByTeamId(reqPayload.teamId.toString()));
+
+    // The value we return becomes the `fulfilled` action payload
+    return response;
+  }
+);
+
+export const deleteUserFromTeamAsync = createAsyncThunk<
+  Types.GenericResponseType<undefined>,
+  Types.DeleteTeamUserReqPayload
+>(
+  '/team/deleteUserFromTeam',
+  async (reqPayload: { teamId: number; profileId: number }, thunkAPI) => {
+    const response = await fetcher.remove<Types.GenericResponseType<undefined>>(
+      `/team/deleteUserFromTeam`,
+      reqPayload
+    );
+
+    if (response.success)
+      thunkAPI.dispatch(getTeamsWithUserByTeamId(reqPayload.teamId.toString()));
+
     // The value we return becomes the `fulfilled` action payload
     return response;
   }
@@ -162,17 +186,26 @@ export const teamSlice = createSlice({
       state,
       action: PayloadAction<{ teamId: number; teamName: string }>
     ) => {
+      // teamName updated > updated teamName in reducer instead of fetching list agian
       state.allTeams.teams = state.allTeams.teams.map((t) => {
         if (t.team_id === action.payload.teamId)
           return { ...t, team_name: action.payload.teamName };
         return t;
       });
     },
-    idleTeamNameUpdateStatus: (state) => {
-      state.updateTeam.status = 'idle';
-    },
     resetTeam: (state) => {
       state.selectedTeamId = -1;
+    },
+    resetCreateTeamState: (state) => {
+      state.teamCreated = undefined;
+    },
+    resetUpdateTeam: (state) => {
+      state.updateTeam.status = 'idle';
+      state.updateTeam.message = undefined;
+    },
+    resetDeleteTeamUser: (state) => {
+      state.deleteTeamUser.status = 'idle';
+      state.deleteTeamUser.message = undefined;
     },
   },
   // The `extraReducers` field lets the slice handle actions defined elsewhere,
@@ -181,31 +214,50 @@ export const teamSlice = createSlice({
     builder
       .addCase(createTeamAsync.pending, (state) => {
         state.teamCreated = {
+          type: 'info',
           status: 'loading',
-          message: undefined,
-          error: undefined,
-          errorMessage: undefined,
+          error: false,
         };
       })
       .addCase(createTeamAsync.fulfilled, (state, action) => {
         state.teamCreated = {
-          message:
-            action.payload.affectedRows === 1
-              ? 'Team created successfully!'
-              : 'Team could not be created',
-          error: action.payload.affectedRows === 1,
-          errorMessage:
-            action.payload.affectedRows !== 1 ? 'Team not created' : undefined,
+          type: 'success',
           status: 'idle',
+          error: false,
+          message: action.payload.message,
         };
       })
-      .addCase(createTeamAsync.rejected, (state) => {
-        state.teamCreated = {
-          message: undefined,
-          error: true,
-          errorMessage: 'Team creation failed!',
-          status: 'failed',
-        };
+      .addCase(
+        createTeamAsync.rejected,
+        (state, action: ReturnType<typeof createTeamAsync.rejected>) => {
+          state.teamCreated = {
+            type: 'error',
+            status: 'failed',
+            error: true,
+            message: action.error.message,
+          };
+        }
+      )
+
+      .addCase(getAllTeams.pending, (state) => {
+        state.TeamWithUser.status = 'loading';
+        state.TeamWithUser.TeamWithUsers = [];
+      })
+      .addCase(getAllTeams.fulfilled, (state, action) => {
+        state.TeamWithUser.status = 'idle';
+        state.allTeams.teams = action.payload.data.map((t) => ({
+          team_id: t.teamId,
+          team_name: t.teamName,
+          created_by_profile_id: t.createdByProfileId,
+          created_by_fname: t.createdByFname,
+          created_by_lname: t.createdByLname,
+          created_by: `${t.createdByFname} ${t.createdByLname}`,
+          users: [],
+        }));
+      })
+      .addCase(getAllTeams.rejected, (state) => {
+        state.TeamWithUser.status = 'failed';
+        state.allTeams.teams = [];
       })
 
       .addCase(getTeamsWithUsersAsync.pending, (state) => {
@@ -219,27 +271,6 @@ export const teamSlice = createSlice({
       .addCase(getTeamsWithUsersAsync.rejected, (state) => {
         state.TeamWithUser.status = 'failed';
         state.TeamWithUser.TeamWithUsers = [];
-      })
-
-      .addCase(getAllTeams.pending, (state) => {
-        state.TeamWithUser.status = 'loading';
-        state.TeamWithUser.TeamWithUsers = [];
-      })
-      .addCase(getAllTeams.fulfilled, (state, action) => {
-        state.TeamWithUser.status = 'idle';
-        state.allTeams.teams = action.payload.map((t) => ({
-          team_id: t.teamId,
-          team_name: t.teamName,
-          created_by_profile_id: t.createdByProfileId,
-          created_by_fname: t.createdByFname,
-          created_by_lname: t.createdByLname,
-          created_by: `${t.createdByFname} ${t.createdByLname}`,
-          users: [],
-        }));
-      })
-      .addCase(getAllTeams.rejected, (state) => {
-        state.TeamWithUser.status = 'failed';
-        state.allTeams.teams = [];
       })
 
       .addCase(getTeamsWithUserByTeamId.pending, (state) => {
@@ -275,12 +306,26 @@ export const teamSlice = createSlice({
         state.updateTeam.status = 'loading';
       })
       .addCase(assignUserRoleInTeamAsync.fulfilled, (state, action) => {
-        state.updateTeam.status = 'success';
+        state.updateTeam.status = action.payload.success ? 'success' : 'failed';
         state.updateTeam.message = action.payload.message;
       })
-      .addCase(assignUserRoleInTeamAsync.rejected, (state) => {
-        state.allTeams.status = 'failed';
-        state.updateTeam.message = 'Failed to update Team Name!';
+      .addCase(assignUserRoleInTeamAsync.rejected, (state, action) => {
+        state.updateTeam.status = 'failed';
+        state.updateTeam.message = action.error.message;
+      })
+
+      .addCase(deleteUserFromTeamAsync.pending, (state) => {
+        state.deleteTeamUser.status = 'loading';
+      })
+      .addCase(deleteUserFromTeamAsync.fulfilled, (state, action) => {
+        state.deleteTeamUser.status = action.payload.success
+          ? 'success'
+          : 'failed';
+        state.deleteTeamUser.message = action.payload.message;
+      })
+      .addCase(deleteUserFromTeamAsync.rejected, (state, action) => {
+        state.deleteTeamUser.status = 'failed';
+        state.deleteTeamUser.message = action.error.message;
       });
   },
 });
@@ -288,8 +333,10 @@ export const teamSlice = createSlice({
 export const {
   setUserToTeam,
   setTeamName,
-  idleTeamNameUpdateStatus,
   resetTeam,
+  resetCreateTeamState,
+  resetUpdateTeam,
+  resetDeleteTeamUser,
 } = teamSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
@@ -300,16 +347,7 @@ export const teamWithUsers = (state: RootState) =>
 export const allTeams = (state: RootState) => state.team.allTeams;
 export const selectedTeamId = (state: RootState) => state.team.selectedTeamId;
 export const updateTeamState = (state: RootState) => state.team.updateTeam;
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-// export const logoutAsync =
-//   (amount: number): AppThunk =>
-//   (dispatch, getState) => {
-//     const currentValue = selectCount(getState());
-//     if (currentValue % 2 === 1) {
-//       dispatch(incrementByAmount(amount));
-//     }
-//   };
+export const teamCreated = (state: RootState) => state.team.teamCreated;
+export const teamReducer = (state: RootState) => state.team;
 
 export default teamSlice.reducer;
