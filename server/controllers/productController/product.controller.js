c6f6ae3fd7f890fcb5facf063f378330746a6f46
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-10-27 22:18:47 +0530

    CustomResponse added for controllers

--- server/controllers/productController/product.controller.js
+++ server/controllers/productController/product.controller.js
@@ -1,43 +1,55 @@
 import * as productService from "../../services/productService/product.service.js";
 import * as teamService from "../../services/teamService/team.service.js";
 import * as formatter from "../../utils/formatter.js";
+import CustomResponse from "../../utils/customResponse.js";
+import { getErrorResponse } from "../../utils/errorResponse.js";
 
 async function createProductController(req, res) {
   const { productName, createdById, teamId } = req.body;
-  // try {
-  //     const data = await productService.createProduct(productName, createdById, teamId);
-  //     res.status(201).json(data);
-  // } catch (error) {
-  //     res.status(500).json({ error: error.message });
-  // }
+  const response = new CustomResponse(
+    false,
+    "Product created successfully.",
+    201
+  );
+
   try {
-    if (productName && createdById && !teamId) {
-      // const userId = await userProfileService.saveUserProfile(fname, lname, role);
-      const data = await productService.createProduct(productName, createdById);
-      res.status(201).json(data);
-    } else if (productName && createdById && teamId) {
+    if (productName && createdById) {
       const data = await productService.createProduct(
         productName,
         createdById,
         teamId
       );
-      res.status(201).json(data);
+      if (data.affectedRows === 0) {
+        response.success = false;
+        response.message = "Failed to created Product.";
+        response.status = 500;
+      }
     } else {
-      res.status(500).json({
-        error: "productName, creator profile id and teamId is mandatory",
-      });
+      response.success = false;
+      response.message = "Bad Request. Pleaes validate the request payload.";
+      response.status = 400;
     }
+    res.status(response.status).json(response);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    const errResponse = getErrorResponse(error, response);
+    res.status(errResponse.status).json(errResponse);
   }
 }
 
 async function getAllProductsController(req, res) {
+  const response = new CustomResponse(true, "", 200, []);
   try {
     const products = await productService.getProducts();
-    res.status(200).json(products);
+    response.data = products;
+    if (!products) {
+      response.success = false;
+      response.message = "Failed to get Products.";
+      response.status = 500;
+    }
+    res.status(response.status).json(response);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    const errResponse = getErrorResponse(error, response);
+    res.status(errResponse.status).json(errResponse);
   }
 }
 
@@ -54,26 +66,37 @@ async function getAllProductsWithTeamController(req, res) {
 async function updateProductController(req, res) {
   const productId = req.params.id;
   const payload = req.body;
-  console.log("updateProduct Controller");
+  const response = new CustomResponse(
+    true,
+    "Product updated successfully",
+    200,
+    []
+  );
   try {
     const success = await productService.updateProduct(payload, productId);
-    if (success) {
-      res
-        .status(200)
-        .json({ message: "Product updated successfully", status: "success" });
-    } else {
-      res.status(404).json({ message: "Product not found", status: "fail" });
+
+    if (!success) {
+      response.success = false;
+      response.message = "product not found.";
+      response.status = 404;
     }
+    res.status(response.status).json(response);
   } catch (error) {
-    res.status(500).json({ message: error.message, status: "fail" });
+    const errResponse = getErrorResponse(error, response);
+    res.status(errResponse.status).json(errResponse);
   }
 }
 
 async function getProductByIdController(req, res) {
   const productId = req.params.id;
+  const response = new CustomResponse(
+    true,
+    "Product with team not found.",
+    404,
+    []
+  );
   try {
     const products = await productService.getProductById(productId);
-    console.log(products);
     const teamId = products[0]?.teamId;
     if (products) {
       const teamWithUsers = await teamService.getTeamWithUsersById(teamId);
@@ -82,13 +105,15 @@ async function getProductByIdController(req, res) {
         formattedProduct,
         teamWithUsers
       );
-
-      res.status(200).json(productWithTeamUsers);
-    } else {
-      res.status(404).json({ error: "Product not found" });
+      response.success = true;
+      response.message = "";
+      response.status = 200;
+      response.data = productWithTeamUsers;
     }
+    res.status(response.status).json(response);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    const errResponse = getErrorResponse(error, response);
+    res.status(errResponse.status).json(errResponse);
   }
 }
 
