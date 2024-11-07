19b6adfec3d88f5b85ec3d676edd8c4d8a236fd6
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-11-07 20:33:01 +0530

    create Epic button for admin

--- client-react-redux/src/pages/epics/Epics.tsx
+++ client-react-redux/src/pages/epics/Epics.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../components/layout/Layout';
+import { AuthUtil } from '../../utils/auth/auth';
+import { Button, Tooltip } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 const Epics: React.FC = () => {
+  const userDetail = AuthUtil.getUserDetail();
+  const isAdmin = userDetail?.roleId && userDetail?.roleId === 1;
+
+  const [showCreateEpic, setShowCreateEpic] = useState<boolean>(false);
+
+  const showCreateEpicSection = () => {
+    setShowCreateEpic(true);
+  };
+
+  const hideCreateEpicSection = () => {
+    setShowCreateEpic(false);
+  };
+
   return (
     <Layout>
+      {isAdmin && !showCreateEpic ? (
+        <div className="mt-3 mb-3">
+          <Tooltip title="Create new team" placement="right">
+            <Button
+              type="primary"
+              icon={<PlusOutlined />}
+              size="middle"
+              onClick={showCreateEpicSection}
+            />
+          </Tooltip>
+        </div>
+      ) : (
+        <div className="mt-3 mb-4">Create Epic</div>
+      )}
       <h2>Epics Page</h2>
     </Layout>
   );
