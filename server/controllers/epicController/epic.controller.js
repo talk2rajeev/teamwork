9270b1df81a844b3fcff86e8cfd34dd3661c1b65
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-11-12 00:34:30 +0530

    user profileId passed to getEpics query

--- server/controllers/epicController/epic.controller.js
+++ server/controllers/epicController/epic.controller.js
@@ -1,64 +1,63 @@
-import * as epicService from '../../services/epicService/epic.service.js';
+import * as epicService from "../../services/epicService/epic.service.js";
 
-async function createEpicController(req,res) {
-    const {epicName, epicDescription, createdById, productId} = req.body;
-    try {
-        const data = await epicService.createEpic({epicName, epicDescription, createdById, productId})
-        res.status(201).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function createEpicController(req, res) {
+  const { epicName, epicDescription, createdById, productId } = req.body;
+  try {
+    const data = await epicService.createEpic({
+      epicName,
+      epicDescription,
+      createdById,
+      productId,
+    });
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function updateEpicController(req,res) {
-    const epicId = req.params.id;
-    try {
-        const data = await epicService.updateEpic({...req.body}, epicId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function updateEpicController(req, res) {
+  const epicId = req.params.id;
+  try {
+    const data = await epicService.updateEpic({ ...req.body }, epicId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getEpicsController(req,res) {
-    try {
-        const data = await epicService.getEpics();
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function getEpicsController(req, res) {
+  try {
+    const data = await epicService.getEpics(req.user.profileId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getEpicsByIdController(req,res) {
-    const epicId = req.params.id;
-    try {
-        const data = await epicService.getEpicById(epicId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function getEpicsByIdController(req, res) {
+  const epicId = req.params.id;
+  try {
+    const data = await epicService.getEpicById(epicId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getEpicsByProductIdController(req,res) {
-    const productId = req.params.id;
-    try {
-        const data = await epicService.getEpicsByProductId(productId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function getEpicsByProductIdController(req, res) {
+  const productId = req.params.id;
+  try {
+    const data = await epicService.getEpicsByProductId(productId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-
-
-
-export { 
-    createEpicController,
-    getEpicsController,
-    updateEpicController,
-    getEpicsByIdController,
-    getEpicsByProductIdController,
+export {
+  createEpicController,
+  getEpicsController,
+  updateEpicController,
+  getEpicsByIdController,
+  getEpicsByProductIdController,
 };
-
-
-
--- server/services/epicService/epic.service.js
+++ server/services/epicService/epic.service.js
@@ -1,59 +1,68 @@
-import {pool} from '../../config/dbConfig/database.js';
-import {SQL_QUERIES} from '../../utils/queries.js';
+import { pool } from "../../config/dbConfig/database.js";
+import { SQL_QUERIES } from "../../utils/queries.js";
 
 async function createEpic(payload) {
-    try{
-        const { epicName, epicDescription, createdById, productId } = payload;
-        const result = await pool.query(SQL_QUERIES.epic.createEpicQuery, [epicName, epicDescription, createdById, productId]);
-        return result[0];
-    } catch(err) {
-        throw new Error(`Failed to create epic: ${err.message}`);
-    };
-    
+  try {
+    const { epicName, epicDescription, createdById, productId } = payload;
+    const result = await pool.query(SQL_QUERIES.epic.createEpicQuery, [
+      epicName,
+      epicDescription,
+      createdById,
+      productId,
+    ]);
+    return result[0];
+  } catch (err) {
+    throw new Error(`Failed to create epic: ${err.message}`);
+  }
 }
 
-async function getEpics() {
-    try{
-        const result = await pool.query(SQL_QUERIES.epic.getEpics);
-        return result[0];
-    } catch(err) {
-        throw new Error(`Failed to get the epics: ${err.message}`);
-    };
-    
+async function getEpics(profileId) {
+  try {
+    const result = await pool.query(SQL_QUERIES.epic.getEpics, [
+      profileId,
+      profileId,
+    ]);
+    return result[0];
+  } catch (err) {
+    throw new Error(`Failed to get the epics: ${err.message}`);
+  }
 }
 
 async function getEpicById(epicId) {
-    try{
-        const result = await pool.query(SQL_QUERIES.epic.getEpicById, [epicId]);
-        return result[0];
-    } catch(err) {
-        throw new Error(`Failed to get epic by epicId: ${err.message}`);
-    };
-    
+  try {
+    const result = await pool.query(SQL_QUERIES.epic.getEpicById, [epicId]);
+    return result[0];
+  } catch (err) {
+    throw new Error(`Failed to get epic by epicId: ${err.message}`);
+  }
 }
 
-async function  updateEpic(epicData, epicId) {
-    try {
-        const query = "Update epic SET " + Object.keys(epicData).map(key => `${key} = ?`).join(',')+ " WHERE epicId = ?";
-        const params = [...Object.values(epicData), epicId];
-        const result = await pool.query(query, params);
-    
-        return result;
-    } catch(err) {
-        throw new Error(`Failed to update Epic: ${err.message}`);
-    };
+async function updateEpic(epicData, epicId) {
+  try {
+    const query =
+      "Update epic SET " +
+      Object.keys(epicData)
+        .map((key) => `${key} = ?`)
+        .join(",") +
+      " WHERE epicId = ?";
+    const params = [...Object.values(epicData), epicId];
+    const result = await pool.query(query, params);
+
+    return result;
+  } catch (err) {
+    throw new Error(`Failed to update Epic: ${err.message}`);
+  }
 }
 
 async function getEpicsByProductId(productId) {
-    try{
-        const result = await pool.query(SQL_QUERIES.epic.getEpicsByProductIdQuery, [productId]);
-        return result[0];
-    } catch(err) {
-        throw new Error(`Failed to get epic by productId: ${err.message}`);
-    };
-    
+  try {
+    const result = await pool.query(SQL_QUERIES.epic.getEpicsByProductIdQuery, [
+      productId,
+    ]);
+    return result[0];
+  } catch (err) {
+    throw new Error(`Failed to get epic by productId: ${err.message}`);
+  }
 }
 
-
-
-export { createEpic, getEpics, getEpicsByProductId, updateEpic, getEpicById};
+export { createEpic, getEpics, getEpicsByProductId, updateEpic, getEpicById };
