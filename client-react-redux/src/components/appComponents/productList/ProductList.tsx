f140358d54be2f52e3633c11f7306ed989bd8332
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-11-13 23:02:43 +0530

    only admin can update

--- client-react-redux/src/components/appComponents/productList/ProductList.tsx
+++ client-react-redux/src/components/appComponents/productList/ProductList.tsx
@@ -2,60 +2,68 @@ import React from 'react';
 import { Space, Table, TableProps } from 'antd';
 import * as Types from '../../../utils/types/types';
 import { IoMdEye, IoMdCreate } from 'react-icons/io';
+import { AuthUtil } from '../../../utils/auth/auth';
 
 const getColumns = (
   viewProductDetail: (
     event: React.MouseEvent<HTMLSpanElement, MouseEvent>
   ) => void,
   updateProduct: (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void
-): TableProps<Types.Product>['columns'] => [
-  {
-    title: 'Product Name',
-    dataIndex: 'productName',
-    key: 'productName',
-    render: (text) => <span>{text}</span>,
-  },
-  {
-    title: 'Product Owner',
-    key: 'productOwner',
-    dataIndex: 'created_by',
-    render: (_, product) => (
-      <Space size="middle">
-        <span>
-          {product.product_owner_fname} {product.product_owner_lname}
-        </span>
-      </Space>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, product) => (
-      <Space size="middle">
-        <span
-          data-action="view"
-          data-productid={product.productId}
-          onClick={viewProductDetail}
-        >
-          <IoMdEye
-            size="16"
-            className="cursor-pointer text-gray-500 hover:text-gray-700"
-          />
-        </span>
-        <span
-          data-action="edit"
-          data-productid={product.productId}
-          onClick={updateProduct}
-        >
-          <IoMdCreate
-            size="16"
-            className="cursor-pointer text-gray-500 hover:text-gray-700"
-          />
-        </span>
-      </Space>
-    ),
-  },
-];
+): TableProps<Types.Product>['columns'] => {
+  const userDetail = AuthUtil.getUserDetail();
+  const isAdmin = userDetail?.roleId && userDetail?.roleId === 1;
+
+  return [
+    {
+      title: 'Product Name',
+      dataIndex: 'productName',
+      key: 'productName',
+      render: (text) => <span>{text}</span>,
+    },
+    {
+      title: 'Product Owner',
+      key: 'productOwner',
+      dataIndex: 'created_by',
+      render: (_, product) => (
+        <Space size="middle">
+          <span>
+            {product.product_owner_fname} {product.product_owner_lname}
+          </span>
+        </Space>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, product) => (
+        <Space size="middle">
+          <span
+            data-action="view"
+            data-productid={product.productId}
+            onClick={viewProductDetail}
+          >
+            <IoMdEye
+              size="16"
+              className="cursor-pointer text-gray-500 hover:text-gray-700"
+            />
+          </span>
+          {(isAdmin || product.product_owner_id === userDetail?.profileId) && (
+            <span
+              data-action="edit"
+              data-productid={product.productId}
+              onClick={updateProduct}
+            >
+              <IoMdCreate
+                size="16"
+                className="cursor-pointer text-gray-500 hover:text-gray-700"
+              />
+            </span>
+          )}
+        </Space>
+      ),
+    },
+  ];
+};
 
 interface ProductListProps {
   productList: Array<Types.Product>;
