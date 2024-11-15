c3f54b53876fa1cd16a0849113e74f72bb9a4d42
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-11-16 00:36:55 +0530

    searchSprint route added

--- server/routes/sprintRoutes/sprintRoutes.js
+++ server/routes/sprintRoutes/sprintRoutes.js
@@ -1,11 +1,21 @@
-import express from 'express';
-import * as sprintController from '../../controllers/sprintController/sprint.controller.js';
+import express from "express";
+import * as sprintController from "../../controllers/sprintController/sprint.controller.js";
 
 const sprintRoutes = express.Router();
 
-sprintRoutes.post('/createSprint', sprintController.createSprintController);
-sprintRoutes.get('/getAllSprints', sprintController.getAllSprintsController);
-sprintRoutes.put('/updateSprint/:id', sprintController.updateSprintController);
-sprintRoutes.get('/getSprintById/:id', sprintController.getSprintByIdController);
-sprintRoutes.get('/getSprintByProductId/:id', sprintController.getSprintByProductIdController);
+sprintRoutes.post("/createSprint", sprintController.createSprintController);
+sprintRoutes.get("/getAllSprints", sprintController.getAllSprintsController);
+sprintRoutes.put("/updateSprint/:id", sprintController.updateSprintController);
+sprintRoutes.get(
+  "/getSprintById/:id",
+  sprintController.getSprintByIdController
+);
+sprintRoutes.get(
+  "/getSprintByProductId/:id",
+  sprintController.getSprintByProductIdController
+);
+sprintRoutes.get(
+  "/searchSprint/:sprintName",
+  sprintController.searchSprintsController
+);
 export { sprintRoutes };
