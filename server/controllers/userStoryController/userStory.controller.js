9c9d8393b7c9d012c798bfcf257d01b20e4f57e3
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-11-20 04:53:39 +0530

    code preetified, getDetailedUserStoriesBySprintIdController response type generic

--- server/controllers/userStoryController/userStory.controller.js
+++ server/controllers/userStoryController/userStory.controller.js
@@ -1,121 +1,155 @@
-import * as userStoryService from '../../services/userStoryService/userStory.service.js';
+import * as userStoryService from "../../services/userStoryService/userStory.service.js";
+import CustomResponse from "../../utils/customResponse.js";
+import { getErrorResponse } from "../../utils/errorResponse.js";
 
-async function createUserStoryController(req,res) {
-    const payload = req.body;
-    try {
-        const {title, description, userStoryId, reporterUserId, productId} = payload;
-        if(title, description, userStoryId, reporterUserId, productId) {
-            const data = await userStoryService.createUserStory(payload);
-            res.status(201).json(data);
-        } else {
-            res.status(400).json({ error: "Bad request, please check your request payload" });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+async function createUserStoryController(req, res) {
+  const payload = req.body;
+  try {
+    const { title, description, userStoryId, reporterUserId, productId } =
+      payload;
+    if ((title, description, userStoryId, reporterUserId, productId)) {
+      const data = await userStoryService.createUserStory(payload);
+      res.status(201).json(data);
+    } else {
+      res
+        .status(400)
+        .json({ error: "Bad request, please check your request payload" });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function createBugController(req,res) {
-    const payload = req.body;
-    try {
-        const {title, description, userStoryId, reporterUserId, productId, userStoryType, priority} = payload;
-        if(title, description, userStoryId, reporterUserId, productId, userStoryType, priority) {
-            const data = await userStoryService.createBug(payload);
-            res.status(201).json(data);
-        } else {
-            res.status(400).json({ error: "Bad request, please check your request payload" });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+async function createBugController(req, res) {
+  const payload = req.body;
+  try {
+    const {
+      title,
+      description,
+      userStoryId,
+      reporterUserId,
+      productId,
+      userStoryType,
+      priority,
+    } = payload;
+    if (
+      (title,
+      description,
+      userStoryId,
+      reporterUserId,
+      productId,
+      userStoryType,
+      priority)
+    ) {
+      const data = await userStoryService.createBug(payload);
+      res.status(201).json(data);
+    } else {
+      res
+        .status(400)
+        .json({ error: "Bad request, please check your request payload" });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function updateUserStoryController(req,res) {
-    const userStoryId = req.params.id;
-    const payload = req.body;
-    try {
-        // const userId = await userProfileService.saveUserProfile(fname, lname, role);
-        const data = await userStoryService.updateUserStory(payload, userStoryId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function updateUserStoryController(req, res) {
+  const userStoryId = req.params.id;
+  const payload = req.body;
+  try {
+    // const userId = await userProfileService.saveUserProfile(fname, lname, role);
+    const data = await userStoryService.updateUserStory(payload, userStoryId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getUserStoriesBySprintIdController(req,res) {
-    const sprintId = req.params.id;
-    try {
-        // const userId = await userProfileService.saveUserProfile(fname, lname, role);
-        const data = await userStoryService.getUserStoriesBySprintId(sprintId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function getUserStoriesBySprintIdController(req, res) {
+  const sprintId = req.params.id;
+  try {
+    // const userId = await userProfileService.saveUserProfile(fname, lname, role);
+    const data = await userStoryService.getUserStoriesBySprintId(sprintId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getUserStoriesByProductIdController(req,res) {
-    const productId = req.params.id;
-    console.log('getting getUserStoriesByProductId ', productId);
-    try {
-        // const userId = await userProfileService.saveUserProfile(fname, lname, role);
-        const data = await userStoryService.getUserStoriesByProductId(productId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    
+async function getUserStoriesByProductIdController(req, res) {
+  const productId = req.params.id;
+  console.log("getting getUserStoriesByProductId ", productId);
+  try {
+    // const userId = await userProfileService.saveUserProfile(fname, lname, role);
+    const data = await userStoryService.getUserStoriesByProductId(productId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getUserStoriesByEpicIdController(req,res) {
-    const epicId = req.params.id;
-    console.log('req.query ', req.query);
-    try {
-        // const userId = await userProfileService.saveUserProfile(fname, lname, role);
-        const data = await userStoryService.getUserStoriesByEpicId(epicId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function getUserStoriesByEpicIdController(req, res) {
+  const epicId = req.params.id;
+  console.log("req.query ", req.query);
+  try {
+    // const userId = await userProfileService.saveUserProfile(fname, lname, role);
+    const data = await userStoryService.getUserStoriesByEpicId(epicId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getDetailedUserStoriesBySprintIdController(req,res) {
-    const sprintId = req.params.id;
-    try {
-        const data = await userStoryService.getDetailedUserStoriesBySprintId(sprintId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+async function getDetailedUserStoriesBySprintIdController(req, res) {
+  const sprintId = req.params.id;
+  const response = new CustomResponse(true, "", 200, []);
+  try {
+    const data = await userStoryService.getDetailedUserStoriesBySprintId(
+      sprintId
+    );
+    response.data = data;
+    if (!data) {
+      response.success = false;
+      response.message = "Failed to get userstories.";
+      response.status = 500;
     }
+    res.status(response.status).json(response);
+  } catch (error) {
+    const errResponse = getErrorResponse(error, response);
+    res.status(errResponse.status).json(errResponse);
+  }
 }
 
-async function getDetailedUserStoriesByProductIdController(req,res) {
-    const productId = req.params.id;
-    try {
-        const data = await userStoryService.getDetailedUserStoriesByProductId(productId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function getDetailedUserStoriesByProductIdController(req, res) {
+  const productId = req.params.id;
+  try {
+    const data = await userStoryService.getDetailedUserStoriesByProductId(
+      productId
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-async function getDetailedUserStoriesByEpicIdController(req,res) {
-    const epicId = req.params.id;
-    try {
-        const data = await userStoryService.getDetailedUserStoriesByEpicId(epicId);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+async function getDetailedUserStoriesByEpicIdController(req, res) {
+  const epicId = req.params.id;
+  try {
+    const data = await userStoryService.getDetailedUserStoriesByEpicId(epicId);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 export {
-    createUserStoryController,
-    createBugController,
-    updateUserStoryController,
-    getUserStoriesBySprintIdController,
-    getUserStoriesByProductIdController,
-    getUserStoriesByEpicIdController,
-    getDetailedUserStoriesBySprintIdController,
-    getDetailedUserStoriesByProductIdController,
-    getDetailedUserStoriesByEpicIdController
-}
+  createUserStoryController,
+  createBugController,
+  updateUserStoryController,
+  getUserStoriesBySprintIdController,
+  getUserStoriesByProductIdController,
+  getUserStoriesByEpicIdController,
+  getDetailedUserStoriesBySprintIdController,
+  getDetailedUserStoriesByProductIdController,
+  getDetailedUserStoriesByEpicIdController,
+};
