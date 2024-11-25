2169151c3bacb0f7bdb653a6a14b0fe0e01faa9a
Rajeev Sharma <talk2rajeev1986@gmail.com>
2024-11-25 23:39:50 +0530

    getDetailedUserStoriesByUserStoryId route

--- server/routes/userStoryRoutes/userStoryRoutes.js
+++ server/routes/userStoryRoutes/userStoryRoutes.js
@@ -1,16 +1,44 @@
-import express from 'express';
-import * as userStoryController from '../../controllers/userStoryController/userStory.controller.js';
+import express from "express";
+import * as userStoryController from "../../controllers/userStoryController/userStory.controller.js";
 
 const userStoryRoutes = express.Router();
 
-userStoryRoutes.post('/createUserStory', userStoryController.createUserStoryController);
-userStoryRoutes.post('/createBug', userStoryController.createBugController);
-userStoryRoutes.put('/updateUserStory/:id', userStoryController.updateUserStoryController);
-userStoryRoutes.get('/getUserStoriesBySprintId/:id', userStoryController.getUserStoriesBySprintIdController);
-userStoryRoutes.get('/getUserStoriesByProductId/:id', userStoryController.getUserStoriesByProductIdController);
-userStoryRoutes.get('/getUserStoriesByEpicId/:id', userStoryController.getUserStoriesByEpicIdController);
-userStoryRoutes.get('/getDetailedUserStoriesBySprintId/:id', userStoryController.getDetailedUserStoriesBySprintIdController);
-userStoryRoutes.get('/getDetailedUserStoriesByProductId/:id', userStoryController.getDetailedUserStoriesByProductIdController);
-userStoryRoutes.get('/getDetailedUserStoriesByEpicId/:id', userStoryController.getDetailedUserStoriesByEpicIdController);
+userStoryRoutes.post(
+  "/createUserStory",
+  userStoryController.createUserStoryController
+);
+userStoryRoutes.post("/createBug", userStoryController.createBugController);
+userStoryRoutes.put(
+  "/updateUserStory/:id",
+  userStoryController.updateUserStoryController
+);
+userStoryRoutes.get(
+  "/getUserStoriesBySprintId/:id",
+  userStoryController.getUserStoriesBySprintIdController
+);
+userStoryRoutes.get(
+  "/getUserStoriesByProductId/:id",
+  userStoryController.getUserStoriesByProductIdController
+);
+userStoryRoutes.get(
+  "/getUserStoriesByEpicId/:id",
+  userStoryController.getUserStoriesByEpicIdController
+);
+userStoryRoutes.get(
+  "/getDetailedUserStoriesByUserStoryId/:productId/:sprintId/:userStoryId",
+  userStoryController.getDetailedUserStoriesByUserStoryIdController
+);
+userStoryRoutes.get(
+  "/getDetailedUserStoriesBySprintId/:productId/:sprintId",
+  userStoryController.getDetailedUserStoriesBySprintIdController
+);
+userStoryRoutes.get(
+  "/getDetailedUserStoriesByProductId/:id",
+  userStoryController.getDetailedUserStoriesByProductIdController
+);
+userStoryRoutes.get(
+  "/getDetailedUserStoriesByEpicId/:id",
+  userStoryController.getDetailedUserStoriesByEpicIdController
+);
 
 export { userStoryRoutes };
